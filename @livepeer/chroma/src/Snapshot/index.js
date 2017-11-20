187233f9c4a3242a8a80ca097a5d856d962ffea9
jozanza <hello@jsavary.com>
2017-11-20 16:44:55 -0500

    feat(chroma): Added caching for Snapshot and better default styles for VideoPlayer

--- @livepeer/chroma/src/Snapshot/index.js
+++ @livepeer/chroma/src/Snapshot/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Hls from 'hls.js'
 
+const CACHE = {}
+
 export default class Snapshot extends Component {
   static defaultProps = {
     as: 'img',
     at: 0,
     defaultSrc: '',
-    maxWidth: 100,
-    maxHeight: 100,
     crop: true,
+    width: 100,
+    height: 100,
+    onSnapshotReady: () => {},
   }
   state = {
     src: '',
@@ -16,30 +19,42 @@ export default class Snapshot extends Component {
   canvas = document.createElement('canvas')
   video = document.createElement('video')
   setThumbnailDataURL = () => {
-    const { canvas, ctx, props, video } = this
+    const { canvas, ctx, key, props, video } = this
     const { videoWidth, videoHeight } = video
-    const { crop, maxWidth, maxHeight } = props
-    const wScale = maxWidth / videoWidth
-    const hScale = maxHeight / videoHeight
-    const scale = Math[crop ? 'max' : 'min'](wScale, hScale)
-    canvas.width = maxWidth
-    canvas.height = maxHeight
-    ctx.drawImage(
-      video,
-      0,
-      0,
-      videoWidth,
-      videoHeight,
-      (maxWidth - videoWidth * scale) / 2,
-      (maxHeight - videoHeight * scale) / 2,
-      videoWidth * scale,
-      videoHeight * scale,
-    )
-    this.setState({ src: canvas.toDataURL() })
+    const { crop, width, height } = props
+    if (!CACHE[key]) {
+      const w = Number(width)
+      const h = Number(height)
+      const wScale = w / videoWidth
+      const hScale = h / videoHeight
+      const scale = Math[crop ? 'max' : 'min'](wScale, hScale)
+      canvas.width = w
+      canvas.height = h
+      ctx.drawImage(
+        video,
+        0,
+        0,
+        videoWidth,
+        videoHeight,
+        (w - videoWidth * scale) / 2,
+        (h - videoHeight * scale) / 2,
+        videoWidth * scale,
+        videoHeight * scale,
+      )
+      CACHE[key] = canvas.toDataURL()
+    }
+    this.setState({ src: CACHE[key] }, () => {
+      this.props.onSnapshotReady(this.state.src)
+    })
     this.video.removeEventListener('canplay', this.setThumbnailDataURL)
     this.hls.destroy()
   }
   updateThumbnail = () => {
+    if (CACHE[this.key]) {
+      return this.setState({ src: CACHE[this.key] }, () => {
+        this.props.onSnapshotReady(this.state.src)
+      })
+    }
     const { canvas, video, props } = this
     const parent = document.createElement('div')
     parent.appendChild(canvas)
@@ -51,26 +66,30 @@ export default class Snapshot extends Component {
     video.currentTime = props.at
     this.video.addEventListener('canplay', this.setThumbnailDataURL)
   }
+  getKey = props => {
+    return JSON.stringify({
+      at: props.at,
+      width: props.width,
+      height: props.height,
+      crop: props.crop,
+      url: props.url,
+    })
+  }
   componentDidMount() {
+    this.key = this.getKey(this.props)
     this.updateThumbnail()
   }
   componentWillUnmount() {
     this.video.removeEventListener('canplay', this.setThumbnailDataURL)
   }
   componentWillReceiveProps(nextProps) {
-    if (nextProps.url === this.props.url) return
+    const nextKey = this.getKey(nextProps)
+    if (this.key === nextKey) return
+    this.key = nextKey
     this.updateThumbnail()
   }
   render() {
-    const {
-      as,
-      at,
-      crop,
-      defaultSrc,
-      maxWidth,
-      maxHeight,
-      ...props
-    } = this.props
+    const { as, at, crop, defaultSrc, onSnapshotReady, ...props } = this.props
     const { src } = this.state
     return React.createElement(as, { ...props, src: src || defaultSrc })
   }
