65e46ad9ef01523afe199942a0ac7456be014a0a
jozanza <hello@jsavary.com>
2017-11-20 11:54:56 -0500

    feat(chroma): Added cropping and scaling to Snapshot component

--- @livepeer/chroma/src/Snapshot/index.js
+++ @livepeer/chroma/src/Snapshot/index.js
@@ -4,7 +4,11 @@ import Hls from 'hls.js'
 export default class Snapshot extends Component {
   static defaultProps = {
     as: 'img',
-    at: 10, // Sometimes videos are blank for the first few secs
+    at: 0,
+    defaultSrc: '',
+    maxWidth: 100,
+    maxHeight: 100,
+    crop: true,
   }
   state = {
     src: '',
@@ -12,29 +16,37 @@ export default class Snapshot extends Component {
   canvas = document.createElement('canvas')
   video = document.createElement('video')
   setThumbnailDataURL = () => {
-    const { canvas, ctx, video } = this
-    canvas.width = video.videoWidth
-    canvas.height = video.videoHeight
-    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
-    this.setState({
-      src: canvas.toDataURL(),
-    })
+    const { canvas, ctx, props, video } = this
+    const { videoWidth, videoHeight } = video
+    const { crop, maxWidth, maxHeight } = props
+    const wScale = maxWidth / videoWidth
+    const hScale = maxHeight / videoHeight
+    const scale = Math[crop ? 'max' : 'min'](wScale, hScale)
+    canvas.width = maxWidth
+    canvas.height = maxHeight
+    ctx.drawImage(
+      video,
+      0,
+      0,
+      videoWidth,
+      videoHeight,
+      (maxWidth - videoWidth * scale) / 2,
+      (maxHeight - videoHeight * scale) / 2,
+      videoWidth * scale,
+      videoHeight * scale,
+    )
+    this.setState({ src: canvas.toDataURL() })
     this.video.removeEventListener('canplay', this.setThumbnailDataURL)
-    this.hls.destroy
+    this.hls.destroy()
   }
   updateThumbnail = () => {
     const { canvas, video, props } = this
     const parent = document.createElement('div')
-    parent.style.position = 'fixed'
-    parent.style.bottom = 0
-    parent.style.right = 0
-    parent.style.transform = `translateX(99999vw)`
-    document.body.appendChild(parent)
     parent.appendChild(canvas)
     parent.appendChild(video)
     this.hls = new Hls()
     this.ctx = canvas.getContext('2d')
-    this.hls.loadSource(props.src)
+    this.hls.loadSource(props.url)
     this.hls.attachMedia(video)
     video.currentTime = props.at
     this.video.addEventListener('canplay', this.setThumbnailDataURL)
@@ -42,13 +54,24 @@ export default class Snapshot extends Component {
   componentDidMount() {
     this.updateThumbnail()
   }
+  componentWillUnmount() {
+    this.video.removeEventListener('canplay', this.setThumbnailDataURL)
+  }
   componentWillReceiveProps(nextProps) {
-    if (nextProps.src === this.props.src) return
+    if (nextProps.url === this.props.url) return
     this.updateThumbnail()
   }
   render() {
-    const { as: El, at, ...props } = this.props
+    const {
+      as,
+      at,
+      crop,
+      defaultSrc,
+      maxWidth,
+      maxHeight,
+      ...props
+    } = this.props
     const { src } = this.state
-    return React.createElement(El, { ...props, src })
+    return React.createElement(as, { ...props, src: src || defaultSrc })
   }
 }
