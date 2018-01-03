f955277bca880b959fab4cf1f74464fb89cbadf5
jozanza <hello@jsavary.com>
2018-01-03 17:10:59 -0500

    feat(chroma): onLive and onDead callbacks
    Player can now detect when a stream is "live" or not by tracking whether the end segment has changed
    recently

--- @livepeer/chroma/src/VideoPlayer/index.js
+++ @livepeer/chroma/src/VideoPlayer/index.js
@@ -4,9 +4,18 @@ import { Player, BigPlayButton } from 'video-react'
 import injectStyles from './styles'
 import Hls from 'hls.js'
 
-// @TODO:
-//   1. Brand video player with <Logo>
-//   2. Add nice empty state :)
+const pLoader = function(config) {
+  const loader = new Hls.DefaultConfig.loader(config)
+  this.abort = () => loader.abort()
+  this.destroy = () => loader.destroy()
+  this.load = (context, config, callbacks) => {
+    let { type, url } = context
+    if (type === 'manifest') {
+      console.log(`Manifest ${url.split('/')[4]} will be loaded.`)
+    }
+    loader.load(context, config, callbacks)
+  }
+}
 
 const getSourceType = src => {
   const types = [
@@ -30,18 +39,18 @@ const isHLS = x => x === 'application/x-mpegURL'
 export default class VideoPlayer extends Component {
   componentDidMount = injectStyles
   render() {
-    const { src, ...props } = this.props
+    const { src, onLive, onDead, ...props } = this.props
     return (
       <Player playsInline {...props}>
         <BigPlayButton position="center" />
-        {src && (
-          <Source
-            isVideoChild
-            autoPlay={props.autoPlay}
-            src={src}
-            type={getSourceType(src)}
-          />
-        )}
+        <Source
+          isVideoChild
+          onLive={onLive}
+          onDead={onDead}
+          autoPlay={props.autoPlay}
+          src={src}
+          type={getSourceType(src)}
+        />
       </Player>
     )
   }
@@ -53,25 +62,65 @@ export class Source extends Component {
     type: PropTypes.string,
     video: PropTypes.object,
   }
+  static defaultProps = {
+    onLive: () => {},
+    onDead: () => {},
+  }
+  hls = new Hls({ pLoader })
+  // resumes play if the video player is already playing when a stream dies
+  resumePlay
+  // latest sequence (.ts) id
+  endSNs = []
+  // if frag level endSN is the same n times in a row, the stream will be considered "dead"
+  endSNThreshold = 4
   updateSource = () => {
-    const { src, type, video, autoPlay } = this.props
+    const { actions, src, type, video } = this.props
     // remove old listeners
     if (this.hls) {
-      this.hls.off(Hls.Events.MANIFEST_PARSED, this.onManifestParsed)
+      this.hls.destroy()
+      this.hls = new Hls({ pLoader })
     }
-    this.hls = new Hls()
     // load hls video source base on hls.js
     if (isHLS(type) && Hls.isSupported()) {
       this.hls.loadSource(src)
-      this.hls.attachMedia(video)
-      this.hls.on(Hls.Events.MANIFEST_PARSED, this.onManifestParsed)
+      this.hls.on(Hls.Events.LEVEL_LOADED, (event, data) => {
+        console.log('end segment:', data.details.endSN)
+        if (this.endSNs.length >= this.endSNThreshold) this.endSNs.shift()
+        this.endSNs.push(data.details.endSN)
+        const END_SNS = new Set(this.endSNs)
+        if (END_SNS.size > 1 && !this.hls.media) {
+          console.log(`
+            ***************
+            STREAM IS LIVE!
+            ***************
+            Attaching media
+          `)
+          this.hls.attachMedia(video)
+          this.props.onLive()
+          if (this.props.autoPlay || this.resumePlay) {
+            delete this.resumePlay
+            video.play().then(() => {
+              actions.handlePlay()
+            })
+          }
+        }
+        if (this.endSNs.length >= this.endSNThreshold && END_SNS.size <= 1) {
+          console.log(`
+            ***************
+            STREAM IS DEAD!
+            ***************
+            Detaching media
+          `)
+          this.resumePlay = !video.paused
+          this.hls.stopLoad()
+          this.hls.detachMedia()
+          this.props.onDead()
+        }
+      })
+      this.hls.on(Hls.Events.ERROR, (event, data) => {
+        console.log(event, data)
+      })
     }
-  }
-  onManifestParsed = () => {
-    this.hls.off(Hls.Events.MANIFEST_PARSED, this.onManifestParsed)
-    delete this.hls
-    if (!this.props.autoPlay) return
-    this.props.video.play()
   }
   componentDidMount() {
     this.updateSource()
@@ -80,8 +129,12 @@ export class Source extends Component {
     const a = this.props.src
     const b = nextProps.src
     if (a === b) return
+    console.log('Will resume play?', this.resumePlay)
+    if (this.resumePlay === undefined) {
+      this.resumePlay = !nextProps.video.paused
+    }
     // if the src changed, update the video player
-    this.updateSource()
+    setTimeout(() => this.updateSource())
   }
   render() {
     return (
