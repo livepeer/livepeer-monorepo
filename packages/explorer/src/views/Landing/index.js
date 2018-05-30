b309ffc78d47e429201f420b37fc87a6b7a3ea5c
jozanza <hello@jsavary.com>
2018-05-29 21:08:19 -0400

    feat(explorer): Updated landing page
    Added an intro video and a couple high-level calls to action with descriptive copy

--- packages/explorer/src/views/Landing/index.js
+++ packages/explorer/src/views/Landing/index.js
@@ -1,85 +1,171 @@
 import React from 'react'
-import styled from 'styled-components'
-import { BasicNavbar, Footer, ScrollToTopOnMount } from '../../components'
-
-const NETWORKS = {
-  1: 'Ethereum Main Network',
-  2: 'Morden, Expanse Mainnet',
-  3: 'Ropsten Test Network',
-  4: 'Rinkeby Test Network',
-  30: 'Rootstock Main Network',
-  31: 'Rootstock Test Network',
-  42: 'Kovan Test Network',
-  61: 'Ethereum Classic Main Network',
-  62: 'Ethereum Classic Test Network',
-  858585: 'Livepeer Test Network',
-}
-const getNetwork = web3 => {
-  return web3 && web3.version
-    ? NETWORKS[web3.version.network] || 'Unknown'
-    : NETWORKS['858585']
-}
+import styled, { keyframes } from 'styled-components'
+import { Icon } from 'rmwc/Icon'
+import {
+  Content,
+  BasicNavbar,
+  Footer,
+  ScrollToTopOnMount,
+} from '../../components'
 
 const Landing = ({ history, ...props }) => (
-  <Container>
-    <ScrollToTopOnMount />
-    <BasicNavbar />
-    <img
-      src={`${process.env.PUBLIC_URL}/wordmark.svg`}
-      width="240"
-      alt="The glorious Livepeer wordmark"
-    />
-    <h3 style={{ letterSpacing: 8 }}>Protocol Explorer</h3>
-    <br />
-    <br />
-    <p>View a Livepeer account by entering an ETH address</p>
-    <div style={{ maxWidth: '100%', width: 480 }}>
-      <SearchBar
-        id="account"
-        type="search"
-        placeholder="example: 0x86a1405f3aede8e904dbd584971ff685e80418cc"
-        onKeyDown={e => {
-          if (e.keyCode !== 13 || !e.target.value) return
-          document.getElementById('account-search-button').click()
+  <React.Fragment>
+    <Container>
+      <ScrollToTopOnMount />
+      <BasicNavbar />
+      <LandingContent width="800px">
+        <img
+          src={`${process.env.PUBLIC_URL}/wordmark.svg`}
+          width="240"
+          alt="The glorious Livepeer wordmark"
+        />
+        <h3 style={{ letterSpacing: 8 }}>Protocol Explorer</h3>
+        <br />
+        <br />
+        <Video src="/static/media/landing.mp4" />
+        <br />
+        <br />
+        <div style={{ display: 'flex' }}>
+          <InfoBox>
+            <h2>
+              <Icon use="event_note" />&nbsp;
+              {`See What's Happening`}
+            </h2>
+            <p>
+              The Livepeer account view shows the recent protocol activity for
+              any Ethereum address. It also gives you deep insight into
+              important protocol metrics.
+            </p>
+            <br />
+            <form
+              style={{ maxWidth: '100%', width: 480 }}
+              onSubmit={e => {
+                e.preventDefault()
+                const data = new FormData(e.target)
+                history.push(`/accounts/${data.get('address')}`)
+              }}
+            >
+              <SearchBar
+                required
+                name="address"
+                type="search"
+                pattern="^0x[a-fA-F0-9]{40}$"
+                placeholder="Enter an ETH account address"
+                onKeyDown={e => {
+                  if (e.keyCode !== 13 || !e.target.value) return
+                  document.getElementById('account-search-button').click()
+                }}
+              />
+              <br />
+              <br />
+              <div style={{ textAlign: 'right' }}>
+                <CTAButton type="submit">View Account</CTAButton>
+              </div>
+            </form>
+          </InfoBox>
+          <InfoBox>
+            <h2>
+              <Icon use="how_to_vote" />&nbsp;
+              {`Play the Delegation Game`}
+            </h2>
+            <p>
+              Bond to delegates to earn newly minted token and fees from
+              transcoder activity, growing your total ownership in the network.
+            </p>
+            <br />
+            <p style={{ textAlign: 'right', margin: 0 }}>
+              <CTAButton
+                big
+                type="submit"
+                onClick={() => {
+                  history.push('/transcoders')
+                }}
+              >
+                View Transcoders
+              </CTAButton>
+            </p>
+          </InfoBox>
+        </div>
+      </LandingContent>
+      <Footer />
+    </Container>
+  </React.Fragment>
+)
+
+class Video extends React.Component {
+  state = { paused: true }
+  togglePlay = async e => {
+    const { target } = e
+    const attr = 'controls'
+    if (!target.paused) {
+      target.removeAttribute(attr)
+      await target.pause()
+      this.setState({ paused: true })
+    } else {
+      target.setAttribute(attr, attr)
+      await target.play()
+      this.setState({ paused: false })
+    }
+  }
+  render() {
+    return (
+      <div
+        style={{
+          position: 'relative',
+          // video is 1080 x 1728
+          paddingBottom: '62.5%',
+          height: 0,
+          boxShadow: '0 1px 20px 0px rgba(0, 0, 0, 0.5)',
+          cursor: 'pointer',
         }}
-      />
-      <br />
-      <br />
-      <div style={{ textAlign: 'right' }}>
-        <SearchButton
-          id="account-search-button"
-          onClick={() => {
-            const { value } = document.getElementById('account')
-            if (!value) return
-            history.push(`/accounts/${value}`)
+      >
+        <video
+          onClick={this.togglePlay}
+          onTouchStart={this.togglePlay}
+          src={this.props.src}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        />
+        <div
+          style={{
+            display: !this.state.paused ? 'none' : 'flex',
+            color: '#fff',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            pointerEvents: 'none',
           }}
         >
-          search
-        </SearchButton>
-      </div>
-      <br />
-      <br />
-      <div style={{ borderRadius: 8, border: '1px solid #fff', padding: 32 }}>
-        <h2 style={{ textDecoration: 'underline' }}>Current Network</h2>
-        <div>
-          <p>{getNetwork(window.web3)}</p>
+          <Icon use="play_circle_outline" style={{ fontSize: 48 }} />
         </div>
-        <h2 style={{ textDecoration: 'underline' }}>ABI Contract Addresses</h2>
-        {Object.entries(window.livepeer.config.contracts).map(
-          ([name, { address }]) => (
-            <div key={name}>
-              <p>{name}</p>
-              <p>
-                <code>{address}</code>
-              </p>
-            </div>
-          ),
-        )}
       </div>
-    </div>
-    <Footer />
-  </Container>
-)
+    )
+  }
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(1rem);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`
+
+const LandingContent = styled(Content)`
+  animation: ${fadeIn} 1s linear 1;
+`
 
 const Container = styled.div`
   display: flex;
@@ -87,9 +173,24 @@ const Container = styled.div`
   align-items: center;
   justify-content: top;
   min-height: 100vh;
-  padding: 80px 32px;
-  background: radial-gradient(at 200% 200%, rgba(0, 235, 136, 0.5), #000 75%);
+  padding: 80px 0;
+  background: linear-gradient(#000, #1b1b1b 320px);
   color: #fff;
+`
+
+const InfoBox = styled.div`
+  display: inline-block;
+  width: 50%;
+  :nth-child(1) {
+    margin-right: 1.5rem;
+  }
+  :nth-child(2) {
+    margin-left: 1.5rem;
+  }
+  h2 {
+    align-items: center;
+    display: inline-flex;
+  }
 `
 
 const SearchBar = styled.input`
@@ -104,10 +205,11 @@ const SearchBar = styled.input`
   -webkit-appearance: textfield;
 `
 
-const SearchButton = styled.button`
+const CTAButton = styled.button`
   display: inline-block;
   color: #fff;
-  padding: 8px 24px;
+  padding: ${({ big }) => (big ? '15px 24px' : '8px 24px')};
+  font-size: ${({ big }) => (big ? '16px' : '12px')};
   border-radius: 4px;
   background: none;
   text-transform: uppercase;
