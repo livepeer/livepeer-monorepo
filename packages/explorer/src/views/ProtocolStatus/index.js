29375199178bd6424b7e20f4563f09679139ba30
palevoo <kud.nikita@gmail.com>
2018-06-30 10:33:48 +0200

    fixed styles, added correct math to get blocksUntilNextRound, changed condition rendering from loading to initialized to avoid refreshing

--- packages/explorer/src/views/ProtocolStatus/index.js
+++ packages/explorer/src/views/ProtocolStatus/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
-import { TextField } from 'rmwc/TextField'
+import styled from 'styled-components'
 
 import enhance from './enhance'
 import { BasicModal } from '../../components'
+import { MathBN } from '../../utils'
 
-const ProtocolStatus = ({ history, currentRound }) => {
+const ProtocolStatus = ({ history, currentRound, currentBlock }) => {
+  const { id } = currentBlock.data
+  const { startBlock, length } = currentRound.data
+  const nextRoundStartBlock = MathBN.add(startBlock, length)
+  const blocksUntilNextRound = MathBN.sub(nextRoundStartBlock - id)
+
   return (
     <BasicModal
       title="Protocol Status"
@@ -12,28 +18,33 @@ const ProtocolStatus = ({ history, currentRound }) => {
         history.push(history.location.pathname)
       }}
     >
-      {currentRound.loading ? null : (
-        <div>
-          <TextField
-            readOnly
-            dense
-            fullwidth
-            label="Current Block:"
-            value={currentRound.data.id}
-          />
-          <TextField
-            readOnly
-            dense
-            fullwidth
-            label="Next Round starts at Block Number:"
-            value={
-              Number(currentRound.data.id) + Number(currentRound.data.length)
-            }
-          />
-        </div>
+      {currentRound.initialized ? null : (
+        <Wrapper>
+          <Row>
+            <LabelColumn>
+              # Blocks Until Round {nextRoundStartBlock}:
+            </LabelColumn>
+            <ValueColumn>{blocksUntilNextRound}</ValueColumn>
+          </Row>
+        </Wrapper>
       )}
     </BasicModal>
   )
 }
-
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+const LabelColumn = styled.p`
+  text-align: right;
+  min-width: 60%;
+`
+const ValueColumn = styled.p`
+  margin-left: 10px;
+  text-align: left;
+`
 export default enhance(ProtocolStatus)
