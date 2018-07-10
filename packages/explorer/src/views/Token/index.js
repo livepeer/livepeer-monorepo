f3094301de6e1cf0f203c8affa0731b06ad8a605
Doug Petkanics <petkanics@gmail.com>
2018-07-10 15:54:05 -0400

    Formatted the token page. FAQ for ledger support. (#146)
    * Formatted the token page and added back the image of the distribution. Added an faq for ledger support.
    
    * Added new video and header to token page.
    
    * Updating the worker ipfs hash for the merklemine because it changed.

--- packages/explorer/src/views/Token/index.js
+++ packages/explorer/src/views/Token/index.js
@@ -24,7 +24,7 @@ const TokenView = ({ ...props }) => {
             stategy="url"
             use="/static/images/lpt-light.svg"
             style={{ width: 32 }}
-          />&nbsp;Livepeer Token Delegation
+          />&nbsp;Livepeer Token Distribution
         </PageHeading>
         <div
           style={{
@@ -37,12 +37,26 @@ const TokenView = ({ ...props }) => {
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <div style={{ width: '50%', paddingRight: 16, paddingTop: 32 }}>
               <p style={{ fontSize: 14 }}>
-                {`Livepeer Token (LPT) holders can participate in our Delegated Proof-of-Stake (DPoS) protocol. If you had greater than 0.1 ETH in your account on March 16th, you can generate 2.44 LPT. Here's how to get started:`}
+                {`Livepeer distributed 6.3 million Livepeer Token (LPT) to the community upon genesis. 2.5 million eligible accounts can each earn 2.4 LPT. There are two ways to get token:`}
               </p>
-              <ol>
-                <li>Mine & Generate Token</li>
-                <li>Bond Your Token to A Transcoder</li>
-              </ol>
+              <p>
+                Slow Start: March 16 - July 26 2018
+                <br />
+                <span style={{ fontSize: 14 }}>
+                  If you had greater than 0.1 ETH in a private key account on
+                  March 16th, you can generate 2.44 LPT now.
+                </span>
+              </p>
+
+              <p>
+                Claim Period: July 26, 2018 until all accounts are mined
+                <br />
+                <span style={{ fontSize: 14 }}>
+                  2.5 Million accounts each with 2.44 LPT will be unclaimed, and
+                  anyone can claim token using this application (live July
+                  26th).
+                </span>
+              </p>
               <Button
                 onClick={() => {
                   window.location.hash = '#/mine'
@@ -69,7 +83,7 @@ const TokenView = ({ ...props }) => {
               }}
             >
               <Video
-                src="/static/media/instructions.mp4"
+                src="/static/media/lpt.mp4"
                 style={{
                   background: '#000',
                   border: '2px solid rgba(255,255,255,.1)',
@@ -103,6 +117,11 @@ const TokenView = ({ ...props }) => {
               text: 'I have a JSON keystore file, how do I get token?',
               id: 'how-keystore',
             },
+            {
+              text:
+                'I have a hardware wallet eligible account. How do I get token?',
+              id: 'how-hardware',
+            },
           ].map(({ text, id }) => {
             return (
               <li key={id} style={{ marginBottom: 8 }}>
@@ -129,26 +148,38 @@ const TokenView = ({ ...props }) => {
           launch, the protocol included rules to generate 10,000,000 initial
           Livepeer token (LPT). Any ETH account holder with 0.1ETH on March
           16th, 2018 are eligible to generate 2.44 LPT as a part of this
-          distribution. Safety: The Livepeer project will NOT be asking you for
-          ETH, BTC, USD, or any money. Livepeer is not selling anything at this
+          distribution.
+        </p>
+        <p>
+          <b>Safety</b>: The Livepeer project will NOT be asking you for ETH,
+          BTC, USD, or any money. Livepeer is not selling anything at this
           point. If someone claiming to represent Livepeer asks you to send them
           any currency, please report this as fraud to fraud@livepeer.org.
-          Amount: 2.44 LPT into each eligible account Eligibility: Any Ethereum
-          account private key holder with greater than 0.1 ETH in their account
-          on March 16th (Ethereum block # 5,264,265) can generate 2.44 LPT token
-          using the application above until August 1st, after which the slow
-          start period ends. Timeframe: During the slow start period which will
-          last through ~August 1st 2018, users can only generate their own token
-          in their own eligible ETH account. After this period, users can
-          generate token in any eligible account, and the amount that they
-          generate for themselves will grow as more time passes Alternate ways
-          to get LPT: Users who wish to participate but don’t meet the threshold
-          can generate token following the 3 month slow start, or can
-          participate in the community campaign to earn LPT Stake and grow: Over
-          time the majority of token will be earned through doing work on the
-          network as a token holder that stakes token or as a transcoder. Once
-          you generate your token, stake towards a transcoder to earn
-          inflationary token every day
+        </p>
+        <p>
+          <b>Amount</b>: 2.44 LPT into each eligible account Eligibility: Any
+          Ethereum account private key holder with greater than 0.1 ETH in their
+          account on March 16th (Ethereum block # 5,264,265) can generate 2.44
+          LPT token using the application above until August 1st, after which
+          the slow start period ends.
+        </p>
+        <p>
+          <b>Timeframe</b>: During the slow start period which will last through
+          ~August 1st 2018, users can only generate their own token in their own
+          eligible ETH account. After this period, users can generate token in
+          any eligible account, and the amount that they generate for themselves
+          will grow as more time passes.
+        </p>
+        <p>
+          <b>Alternate ways to get LPT</b>: Users who wish to participate but
+          don’t meet the threshold can generate token following the 3 month slow
+          start, or can participate in the community campaign to earn LPT.
+        </p>
+        <p>
+          <b>Stake and grow</b>: Over time the majority of token will be earned
+          through doing work on the network as a token holder that stakes token
+          or as a transcoder. Once you generate your token, stake towards a
+          transcoder to earn inflationary token every day.
         </p>
         <hr style={{ border: '1px solid #ccc', width: '100%' }} />
         <h3 style={{ fontWeight: '400' }} id="am-i-eligible">
@@ -171,19 +202,47 @@ const TokenView = ({ ...props }) => {
           and community in a gradual rollout over the next 36 months. Ownership
           in the network will be decentralized and proportional from the get go,
           with open access to the Livepeer token for the community being baked
-          in from day one. Core team and pre-sale participants will be vesting
-          over time, such that no centralized parties ever control a majority of
-          the distributed token used for staking within the Livepeer network.
-          The token pool will be distributed as follows: Founders and early
-          team: 12.35% — vesting over 36 months from network launch. Pre-sale
-          purchasers: 19% — vesting over 18 months from network launch.
-          Purchasers have facilitated 3 years of runway for Livepeer’s lean,
-          engineering focused core team. Crowd: 63.437% — generated over 3–18
-          months using the MerkleMine algorithm. Grant: 0.213% — immediate
-          issued to a couple early advisors and contributors who helped Livepeer
-          get off the ground. Long term project endowment: 5% — to be used to
-          ensure the longevity of the Livepeer project. The estimated token
-          generation over the next 36 months looks as follows: IMAGE
+          in from day one.
+        </p>
+        <p>
+          Core team and pre-sale participants will be vesting over time, such
+          that no centralized parties ever control a majority of the distributed
+          token used for staking within the Livepeer network. The token pool
+          will be distributed as follows:
+        </p>
+        <ul>
+          <li>
+            <b>Founders and early team</b>: 12.35% — vesting over 36 months from
+            network launch.
+          </li>
+          <li>
+            <b>Pre-sale purchasers</b>: 19% — vesting over 18 months from
+            network launch. Purchasers have facilitated 3 years of runway for
+            Livepeer’s lean, engineering focused core team.
+          </li>
+          <li>
+            <b>Crowd</b>: 63.437% — generated over 3–18 months using the
+            MerkleMine algorithm.
+          </li>
+          <li>
+            <b>Grant</b>: 0.213% — immediate issued to a couple early advisors
+            and contributors who helped Livepeer get off the ground.
+          </li>
+          <li>
+            <b>Long term project endowment</b>: 5% — to be used to ensure the
+            longevity of the Livepeer project.
+          </li>
+        </ul>
+
+        <p>
+          The estimated token generation over the next 36 months looks as
+          follows:
+        </p>
+        <p>
+          <img
+            src="https://livepeer-assets.s3.amazonaws.com/token-release.png"
+            alt="distribution"
+          />
         </p>
         <hr style={{ border: '1px solid #ccc', width: '100%' }} />
         <h3 style={{ fontWeight: '400' }} id="i-want-more">
@@ -192,33 +251,41 @@ const TokenView = ({ ...props }) => {
         <p>
           Not eligible for the slow start period? Join the Decentralized
           Livepeer Community Node Telegram group to earn LPT, as well as
-          community calls, meetups, hackathons, grant programs and more Claiming
-          the remaining token In August, the slow start period ends and anyone
-          can generate token from the remaining ~6.3MM ETH Accounts with LPT
-          that has not been claimed
+          community calls, meetups, hackathons, grant programs and more. In
+          August, the slow start period ends and anyone can generate token from
+          the remaining ~6.3MM ETH Accounts with LPT that has not been claimed.
         </p>
         <hr style={{ border: '1px solid #ccc', width: '100%' }} />
         <h3 style={{ fontWeight: '400' }} id="how-keystore">
           I have a JSON Keystore File, how do I get token?
         </h3>
         <p>
-          If you do not want to use the DApp, you can also use the command line
-          in two ways: Merkle Mine Command Line Scripts. If you have a JSON
-          Keystore file for your eligible account, and you would like to use the
-          command line to generate your transaction: Use the MerkleMine command
-          line script included in this github repository
-          (https://github.com/livepeer/merkle-mine) to generate and submit your
-          proof to the MerkleMine generate transaction. This may be useful if
-          you’d like to automate MerkleMining across many accounts. Having
-          Trouble? Check this walkthrough first
+          If you do not want to use the DApp, and you have a JSON keystore file
+          for your eligible account, you can use the MerkleMine command line
+          script included in{' '}
+          <a href="https://github.com/livepeer/merkle-mine">
+            this github repository
+          </a>
+          to generate and submit your proof to the MerkleMine generate
+          transaction. This may be useful if you’d like to automate MerkleMining
+          across many accounts.
+        </p>
+        <p>
+          Having Trouble? Check this walkthrough first
           (https://forum.livepeer.org/t/common-troubleshooting-questions-merklemine-client-scripts/206)
-          You want to choose your own method for generating and signing the
-          transaction containing your proof: Follow the
-          SPEC(https://github.com/livepeer/merkle-mine/blob/master/SPEC.md).
-          Those looking to choose their own method for key security and
-          transaction signing are encouraged to follow the algorithm to generate
-          proofs, sign transactions offline, and submit using their tool of
-          choice to generate token.
+        </p>
+
+        <hr style={{ border: '1px solid #ccc', width: '100%' }} />
+        <h3 style={{ fontWeight: '400' }} id="how-hardware">
+          I have a hardware wallet eligible account. How do I get token?
+        </h3>
+        <p>
+          See{' '}
+          <a href="https://forum.livepeer.org/t/how-to-generate-livepeer-token-using-mycrypto-myetherwallet-ledger-trezor-support/209">
+            this forum post
+          </a>
+          for instructions on how to generate token using a Ledger Wallet and
+          MyCrypto.
         </p>
       </Content>
     </React.Fragment>
