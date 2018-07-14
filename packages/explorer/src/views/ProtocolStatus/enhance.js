ec47d70f393887b3b35c87b704594b7cc4c07f64
Nikita Kudryavtsev <kud.nikita@gmail.com>
2018-07-14 07:25:09 +0300

    Protocol Status Closes #134 (#139)
    * added route to protocol status modal component
    
    * added conditional onclick link to protocol status modal and history prop
    
    * added Protocol Status modal component with styles and enhanced with connectCurrentRoundQuery
    
    * removed unused BasicModal import
    
    * added conditional onclick link to protocol status modal and history prop
    
    * removed unused BasicModal import
    
    * added cunnectCurrentBlockQuery with compose
    
    * fixed styles, added correct math to get blocksUntilNextRound, changed condition rendering from loading to initialized to avoid refreshing
    
    * refactor(explorer): Move initialize round button into status modal
    
    re #134

---
+++ packages/explorer/src/views/ProtocolStatus/enhance.js
@@ -0,0 +1,46 @@
+import { compose, withHandlers } from 'recompose'
+import {
+  connectCoinbaseQuery,
+  connectCurrentBlockQuery,
+  connectCurrentRoundQuery,
+  connectToasts,
+} from '../../enhancers'
+
+const mapMutationHandlers = withHandlers({
+  initializeRound: ({ toasts }) => async () => {
+    try {
+      toasts.push({
+        id: 'initialize-round',
+        title: 'Initializing round...',
+        body: 'The current round is being initialized.',
+      })
+      // TODO: move into graphql schema as mutation
+      await window.livepeer.rpc.initializeRound()
+      toasts.push({
+        id: 'initialize-round',
+        type: 'success',
+        title: 'Initialization complete',
+        body: 'The current round is now initialized.',
+      })
+    } catch (err) {
+      if (!/User denied/.test(err.message)) {
+        toasts.push({
+          id: 'initialize-round',
+          type: 'error',
+          title: 'Initialization failed',
+          body: err.message,
+        })
+      } else {
+        toasts.dismiss('initialize-round')
+      }
+    }
+  },
+})
+
+export default compose(
+  connectCoinbaseQuery,
+  connectCurrentBlockQuery,
+  connectCurrentRoundQuery,
+  connectToasts,
+  mapMutationHandlers,
+)
