30363201588ee813bc864082d7e478a362bd85b3
Randy1Burrell <Randy1burrell@yahoo.com>
2018-09-30 20:26:49 -0500

    Added alert for locked wallet

--- packages/explorer/src/views/Transcoders/index.js
+++ packages/explorer/src/views/Transcoders/index.js
@@ -57,6 +57,7 @@ const TranscodersView: React.ComponentType<TranscodersViewProps> = ({
   const asc = order === 'asc'
   const total = transcoders.data.length
   const compareFn = createCompareFunction(asc, sort)
+  const locked = window.livepeer.config.accounts.length <= 0
   return (
     <React.Fragment>
       <ScrollToTopOnMount />
@@ -66,6 +67,26 @@ const TranscodersView: React.ComponentType<TranscodersViewProps> = ({
           <CpuIcon size={32} />&nbsp;Transcoders
         </PageHeading>
       </Banner>
+    {
+      locked && (
+        <div
+          style={{
+              textAlign: "center",
+              backgroundColor: "#ea002a",
+              fontSize:"13px",
+              padding: "2px",
+          }}>
+          <p style={{margin: "0", color: "white"}}>
+            Web3 locked. Please unlock&nbsp;
+            <a href="https://metamask.io/?utm_source=livepeer.org&utm_medium=referral"
+              style={{color: "inherit"}}
+            >
+              Metamask
+            </a>
+          </p>
+        </div>
+      )
+    }
       <Content>
         {!total ? null : (
           <InlineHint flag="transcoders-list">
