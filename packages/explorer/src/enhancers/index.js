1af1cf7ef9ea8a98b459a397f5a7d79975210fae
Eli Mallon <eli@stream.place>
2019-02-15 16:34:26 -0500

    Implement error handling/display for transcoder loading (#333)
    Fixes #330
    
    All the `notifyOnNetworkStatusChange: true` changes are necessary to get
    the "error" field of a graphQL query propagating all the way back to our
    component, unfortunately. See discussion here:
    https://github.com/livepeer/livepeerjs/issues/330
    
    Also bumped all the Apollo versions. Didn't fix the problem but it also
    didn't introduce any so I kept the upgrade.

--- packages/explorer/src/enhancers/index.js
+++ packages/explorer/src/enhancers/index.js
@@ -194,6 +194,7 @@ export const connectCoinbaseQuery = graphql(CoinbaseQuery, {
     // this query doesn't touch the network, so we can run it often
     pollInterval: 1000,
     variables: {},
+    notifyOnNetworkStatusChange: true,
   }),
 })
 
@@ -223,6 +224,7 @@ export const connectCurrentBlockQuery = graphql(CurrentBlockQuery, {
   options: ({ match }) => ({
     pollInterval: 15000,
     variables: {},
+    notifyOnNetworkStatusChange: true,
   }),
 })
 
@@ -256,6 +258,7 @@ export const connectCurrentRoundQuery = graphql(CurrentRoundQuery, {
   options: ({ match }) => ({
     pollInterval: 3000,
     variables: {},
+    notifyOnNetworkStatusChange: true,
   }),
 })
 
@@ -290,6 +293,7 @@ export const connectProtocolQuery = graphql(ProtocolQuery, {
   options: ({ match }) => ({
     pollInterval: 5 * 1000,
     variables: {},
+    notifyOnNetworkStatusChange: true,
   }),
 })
 
@@ -467,6 +471,7 @@ export const connectTranscodersQuery = graphql(TranscodersQuery, {
       first: 100,
     },
     fetchPolicy: 'cache-and-network',
+    notifyOnNetworkStatusChange: true,
   }),
 })
 
@@ -528,6 +533,7 @@ export const connectAccountDelegatorUnbondLockQuery = graphql(
       } = location
 
       return {
+        notifyOnNetworkStatusChange: true,
         variables: {
           id: accountId,
           lockId: match.params.lockId,
@@ -588,6 +594,7 @@ export const connectAccountDelegatorQuery = graphql(AccountDelegatorQuery, {
     return result
   },
   options: ({ match }) => ({
+    notifyOnNetworkStatusChange: true,
     pollInterval: 5000,
     variables: {
       id: match.params.accountId,
