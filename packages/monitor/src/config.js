cb83097b1aa1e210a0547d741e76da04476e206f
Eli Mallon <eli@livepeer.org>
2019-04-15 17:23:14 -0700

    Livepeer Subgraph Monitor

---
+++ packages/monitor/src/config.js
@@ -0,0 +1,5 @@
+export * from './discord-notification-url.json'
+
+export const SUBGRAPH_ID = 'Qmdfkii4bvs7JUujaReouAqSNNNycEsEUmTTcyrkMxk9ZU'
+export const SUBGRAPH_URL = 'https://graph.livepeer.org/subgraphs'
+export const DISCORD_USER = '150800399576596480' // Eli
--- packages/monitor/src/monitor.js
+++ packages/monitor/src/monitor.js
@@ -1,64 +1,63 @@
-import fetch from "isomorphic-fetch";
+import fetch from 'isomorphic-fetch'
+import { SUBGRAPH_ID, SUBGRAPH_URL, DISCORD_URL } from './config'
 
-const NOTIFICATION_THRESHOLD = 5;
+const NOTIFICATION_THRESHOLD = 5
 
 const query = `
   query deployment {
-    subgraphDeployment(id: "QmRqkn956oCoKRNY7sGzg7HoMLjDBgUG16pgA83cxGSyxz") {
+    subgraphDeployment(id: "${SUBGRAPH_ID}") {
       latestEthereumBlockHash
       latestEthereumBlockNumber
     }
   }
-`;
+`
 
 export async function getGraphBlock() {
-  const res = await fetch("https://graph.livepeer.org/subgraphs", {
-    method: "POST",
+  const res = await fetch(SUBGRAPH_URL, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
     body: JSON.stringify({
-      query
-    })
-  });
-  const { data } = await res.json();
+      query,
+    }),
+  })
+  const { data } = await res.json()
   const {
     latestEthereumBlockHash,
-    latestEthereumBlockNumber
-  } = data.subgraphDeployment;
-  return [parseInt(latestEthereumBlockNumber), latestEthereumBlockHash];
+    latestEthereumBlockNumber,
+  } = data.subgraphDeployment
+  return [parseInt(latestEthereumBlockNumber), latestEthereumBlockHash]
 }
 
 export async function discordNotification(content) {
-  const res = await fetch(
-    "https://discordapp.com/api/webhooks/563852076615073792/NuijhpwYle3T51fG0Lx2X9VjL2nrN9AxtfAz5D6bTvt4A4eZPhRibo2rBBc46b3l475i/slack",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({ text: content })
-    }
-  );
-  console.log(await res.text());
+  // Note that DISCORD_URL should be Discord's "Slack" format messages
+  const res = await fetch(DISCORD_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ text: content }),
+  })
+  await res.text()
 }
 
 export async function getPublicBlock() {
-  const res = await fetch("https://api.blockcypher.com/v1/eth/main");
-  const { height, hash } = await res.json();
-  return [height, hash];
+  const res = await fetch('https://api.blockcypher.com/v1/eth/main')
+  const { height, hash } = await res.json()
+  return [height, hash]
 }
 
 export async function poll() {
-  const [lpNumber, lpHash] = await getGraphBlock();
-  const [publicNumber, publicHash] = await getPublicBlock();
-  const delta = publicNumber - lpNumber;
-  if (delta > NOTIFICATION_THRESHOLD) {
-    const message = `testing`;
+  const [lpNumber, lpHash] = await getGraphBlock()
+  const [publicNumber, publicHash] = await getPublicBlock()
+  const delta = publicNumber - lpNumber
+  if (delta >= NOTIFICATION_THRESHOLD) {
+    const message = `testing`
     await discordNotification(
-      `Livepeer subgraph is currently ${delta} blocks behind. <@150800399576596480>, you should look into that.`
-    );
+      `Livepeer subgraph is currently ${delta} blocks behind. <${DISCORD_USER}>, you should look into that. Most recent block: https://etherscan.io/block/${lpNumber}`,
+    )
   }
 }
