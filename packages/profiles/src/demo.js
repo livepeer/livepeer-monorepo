8611c63e99cb65798eec7cbd7d275a93d37b8f93
Connor O'Hara <connor@s1nus.com>
2019-07-02 13:43:13 -0400

    feat(profile): fixed minor things for code review

--- packages/profiles/src/demo.js
+++ packages/profiles/src/demo.js
@@ -1,6 +1,6 @@
-import TestComponent from "./TestComponent";
-import ReactDOM from "react-dom";
-import React from "react";
+import TestComponent from './TestComponent'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-const element = <TestComponent />;
-ReactDOM.render(element, document.querySelector("#react-div"));
+const element = <TestComponent />
+ReactDOM.render(element, document.querySelector('#react-div'))
--- packages/profiles/src/index.js
+++ packages/profiles/src/index.js
@@ -1,1 +1,1 @@
-export { default as TestComponent } from "./TestComponent";
+export { default as TestComponent } from './TestComponent'
