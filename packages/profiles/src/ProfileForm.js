58bcf1398c3600df6f8965c7fd919eebde596517
Connor O'Hara <connor@s1nus.com>
2019-07-10 01:43:00 -0400

    feat(profile): trying to get form to play nice with functional style

--- packages/profiles/src/ProfileForm.js
+++ packages/profiles/src/ProfileForm.js
@@ -1,30 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Button from './Button'
 
 const ProfileForm = styled.div``
 
 export default ({ name = '', description = '', url = '' }) => {
-  return (
-    <div>
-      <label>Upload profile picture: </label>
-      <br />
-      <input type="file" />
-      <br />
-      <label>Name: </label>
-      <br />
-      <input type="text" defaultValue={name} />
-      <br />
-      <label>Bio / Description</label>
-      <br />
-      <textarea defaultValue={description} />
-      <br />
-      <label>URL:</label>
-      <br />
-      <input type="url" defaultValue={url} />
-      <br />
-      <Button>Cancel</Button>
-      <Button>Save</Button>
-    </div>
-  )
+  const [formName, setFormName] = useState('')
+  const [content, setContent] = useState('')
+
+  useEffect(() => {
+    console.log('use effect triggered in prof form')
+    setContent(
+      <div>
+        <label>Upload profile picture: </label>
+        <br />
+        <input type="file" />
+        <br />
+        <label>Name: </label>
+        <br />
+        /* * Probably going to want this: *
+        https://stackoverflow.com/questions/37266411/react-stateless-component-this-refs-value
+        */
+        <input type="text" defaultValue={name} />
+        <br />
+        <label>Bio / Description</label>
+        <br />
+        <textarea defaultValue={description} />
+        <br />
+        <label>URL:</label>
+        <br />
+        <input type="url" defaultValue={url} />
+        <br />
+        <Button>Cancel</Button>
+        <Button>Save</Button>
+      </div>,
+    )
+  })
+
+  return content
 }
