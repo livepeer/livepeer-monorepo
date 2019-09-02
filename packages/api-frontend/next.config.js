998aa25efa128e8c62a078c38fc874fd91836ac7
Eli Mallon <eli@livepeer.org>
2019-09-01 21:38:57 -0700

    chore(api-frontend): first attempt at unified api-frontend

---
+++ packages/api-frontend/next.config.js
@@ -0,0 +1,22 @@
+const withTM = require("next-transpile-modules");
+const path = require("path");
+
+module.exports = withTM({
+  // target: "serverless",
+  // transpileModules: ["next-mui-helper"],
+  webpack: (config, { webpack }) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader"
+    });
+    config.resolve.alias["scrypt.js"] = path.resolve(
+      __dirname,
+      "./node_modules/scrypt.js/js.js"
+    );
+    // https://spectrum.chat/zeit/general/error-while-building-project~cf3cadfd-6370-4dae-8399-84c8ab2b4841?m=MTU2MDI1NTY2NDYxNw==
+    config.plugins.push(new webpack.IgnorePlugin(/^(?:electron|ws)$/));
+    // config.resolve.alias['scrypt.js'] = 'js-scrypt'
+    return config;
+  }
+});
--- packages/explorer-streamflow/pages/index.tsx
+++ packages/explorer-streamflow/pages/index.tsx
@@ -1,50 +1,52 @@
-/** @jsx jsx */
-import gql from 'graphql-tag'
-import { useContext, useEffect, useState } from 'react'
-import * as React from 'react'
-import MetaMaskContext from '../lib/metamask'
-import { useQuery } from '@apollo/react-hooks'
-import { jsx, Flex } from 'theme-ui'
-import Layout from '../components/Layout'
-import Table from '../components/Table'
-import ROICalculator from '../components/ROICalculator'
+// /** @jsx jsx */
+import gql from "graphql-tag";
+import { useContext, useEffect, useState } from "react";
+import * as React from "react";
+import MetaMaskContext from "../lib/metamask";
+import { useQuery } from "@apollo/react-hooks";
+import { jsx, Flex } from "theme-ui";
+import Layout from "../components/Layout";
+import Table from "../components/Table";
+import ROICalculator from "../components/ROICalculator";
 
-const GET_DATA = require('../queries/transcoders.graphql')
+const GET_DATA = require("../queries/transcoders.graphql");
 
 export default () => {
   const { data, loading } = useQuery(GET_DATA, {
     notifyOnNetworkStatusChange: true,
     ssr: false
-  })
+  });
 
   if (loading) {
     return (
       <Layout>
         <Flex
           sx={{
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
           Loading...
         </Flex>
       </Layout>
-    )
+    );
   }
   return (
     <Layout>
       <Flex
         sx={{
-          width: 'calc(100% - 256px)',
+          width: "calc(100% - 256px)",
           px: 5
-        }}>
+        }}
+      >
         <>
-          <Flex sx={{ paddingTop: 5, pr: 6, width: '70%' }}>
+          <Flex sx={{ paddingTop: 5, pr: 6, width: "70%" }}>
             <Table transcoders={data.transcoders} />
           </Flex>
           <ROICalculator protocol={data.protocol} />
         </>
       </Flex>
     </Layout>
-  )
-}
+  );
+};
