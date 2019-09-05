86d896d3600b30f86912080fcd411c1df5808d2e
adamsoffer <adam@livepeer.org>
2019-09-05 12:14:34 -0400

    update gitignore file

--- packages/explorer-streamflow/pages/index.tsx
+++ packages/explorer-streamflow/pages/index.tsx
@@ -6,8 +6,28 @@ import Table from '../components/Table'
 import StakingWidget from '../components/StakingWidget'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { withApollo } from '../lib/apollo'
+import gql from 'graphql-tag'
 
-const GET_DATA = require('../queries/transcoders.graphql')
+const GET_DATA = gql`
+  query {
+    transcoders(where: { status: "Registered" }) {
+      id
+      active
+      feeShare
+      rewardCut
+      status
+      totalStake
+      pricePerSegment
+      rewards {
+        rewardTokens
+      }
+    }
+    protocol {
+      totalTokenSupply
+      totalBondedToken
+    }
+  }
+`
 
 export default withApollo(() => {
   const { data, loading } = useQuery(GET_DATA, {
