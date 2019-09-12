e3cc227b64d605c414c61db6a69b6d707c1fed96
adamsoffer <adam@livepeer.org>
2019-09-11 22:40:35 -0400

    fix share calculation

--- packages/subgraph/src/mappings/bondingManager.ts
+++ packages/subgraph/src/mappings/bondingManager.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from '@graphprotocol/graph-ts'
 
 // Import event types from the registrar contract ABIs
 import {
@@ -6,165 +6,208 @@ import {
   Bond,
   Unbond,
   WithdrawStake
-} from "../types/BondingManager/BondingManager";
+} from '../types/BondingManager/BondingManager'
 
 // Import entity types generated from the GraphQL schema
-import { Transcoder, Delegator } from "../types/schema";
-import { RoundsManager } from "../types/RoundsManager/RoundsManager";
+import { Transcoder, Delegator, Share, Pool } from '../types/schema'
+import { RoundsManager } from '../types/RoundsManager/RoundsManager'
+
+import { makePoolId, makeShareId } from './util'
 
 // Bind RoundsManager contract
 let roundsManager = RoundsManager.bind(
-  Address.fromString("3984fc4ceeef1739135476f625d36d6c35c40dc3")
-);
+  Address.fromString('3984fc4ceeef1739135476f625d36d6c35c40dc3')
+)
 
 export function bond(event: Bond): void {
-  let bondingManager = BondingManager.bind(event.address);
-  let transcoderAddress = event.params.delegate;
-  let delegatorAddress = event.params.delegator;
+  let bondingManager = BondingManager.bind(event.address)
+  let transcoderAddress = event.params.delegate
+  let delegatorAddress = event.params.delegator
   let transcoderTotalStake = bondingManager.transcoderTotalStake(
     transcoderAddress
-  );
-  let currentRound = roundsManager.currentRound();
-
-  // Get delegator data
-  let delegatorData = bondingManager.getDelegator(delegatorAddress);
+  )
+  let currentRound = roundsManager.currentRound()
 
   // Create transcoder if it does not yet exist
-  let transcoder = Transcoder.load(transcoderAddress.toHex());
+  let transcoder = Transcoder.load(transcoderAddress.toHex())
   if (transcoder == null) {
-    transcoder = new Transcoder(transcoderAddress.toHex());
+    transcoder = new Transcoder(transcoderAddress.toHex())
   }
 
   // Create delegate if it does not yet exist
-  let delegate = Delegator.load(transcoderAddress.toHex());
+  let delegate = Delegator.load(transcoderAddress.toHex())
   if (delegate == null) {
-    delegate = new Delegator(transcoderAddress.toHex());
+    delegate = new Delegator(transcoderAddress.toHex())
   }
 
   // Get delegate data
-  let delegateData = bondingManager.getDelegator(transcoderAddress);
+  let delegateData = bondingManager.getDelegator(transcoderAddress)
 
   // Update delegate's delegated amount
-  delegate.delegatedAmount = delegateData.value3;
+  delegate.delegatedAmount = delegateData.value3
 
   // Create delegator if it does not yet exist
-  let delegator = Delegator.load(delegatorAddress.toHex());
+  let delegator = Delegator.load(delegatorAddress.toHex()) as Delegator
   if (delegator == null) {
-    delegator = new Delegator(delegatorAddress.toHex());
+    delegator = new Delegator(delegatorAddress.toHex())
   }
 
   if (transcoder.delegators == null) {
-    transcoder.delegators = new Array<string>();
+    transcoder.delegators = new Array<string>()
   }
 
   // Add delegator to delegate if it's not already
-  let delegators = transcoder.delegators;
-  let i = delegators.indexOf(delegatorAddress.toHex());
+  let delegators = transcoder.delegators
+  let i = delegators.indexOf(delegatorAddress.toHex())
   if (i == -1) {
-    delegators.push(delegatorAddress.toHex());
-    transcoder.delegators = delegators;
+    delegators.push(delegatorAddress.toHex())
+    transcoder.delegators = delegators
   }
 
   // Update delegate's total stake
-  transcoder.totalStake = transcoderTotalStake;
+  transcoder.totalStake = transcoderTotalStake
 
-  delegator.delegate = transcoderAddress.toHex();
+  delegator.delegate = transcoderAddress.toHex()
 
   // Update delegator's start round
-  delegator.startRound = currentRound.plus(BigInt.fromI32(1)).toString();
+  delegator.startRound = currentRound.plus(BigInt.fromI32(1)).toString()
 
-  // Update delegator's last claim round
-  delegator.lastClaimRound = currentRound.toString();
+  delegate.save()
+  delegator.save()
+  transcoder.save()
 
-  // Update delegator's bonded amount
-  delegator.bondedAmount = delegatorData.value0;
-
-  // Update delegator's fees
-  delegator.fees = delegator.pendingFees;
-
-  // Update delegator's pending stake
-  delegator.pendingStake = BigInt.fromI32(0);
-
-  // Update delegator's pending fees
-  delegator.pendingFees = BigInt.fromI32(0);
-
-  delegate.save();
-  delegator.save();
-  transcoder.save();
+  updateDelegatorWithEarnings(
+    delegator,
+    currentRound,
+    delegator.pendingStake as BigInt,
+    delegator.pendingFees as BigInt
+  )
 }
 
 export function unbond(event: Unbond): void {
-  let bondingManager = BondingManager.bind(event.address);
-  let delegatorAddress = event.params.delegator;
-  let delegator = Delegator.load(delegatorAddress.toHex());
-  let currentRound = roundsManager.currentRound();
+  let bondingManager = BondingManager.bind(event.address)
+  let delegatorAddress = event.params.delegator
+  let delegator = Delegator.load(delegatorAddress.toHex()) as Delegator
+  let currentRound = roundsManager.currentRound()
 
   // delegate is not emitted in the deprecated event so grab it from delegator
-  let transcoderAddress = Address.fromString(delegator.delegate);
+  let transcoderAddress = Address.fromString(delegator.delegate)
 
-  let transcoder = Transcoder.load(transcoderAddress.toHex());
+  let transcoder = Transcoder.load(transcoderAddress.toHex())
   if (transcoder == null) {
-    transcoder = new Transcoder(transcoderAddress.toHex());
+    transcoder = new Transcoder(transcoderAddress.toHex())
   }
 
-  let delegate = Delegator.load(transcoderAddress.toHex());
+  let delegate = Delegator.load(transcoderAddress.toHex())
   if (delegate == null) {
-    delegate = new Delegator(transcoderAddress.toHex());
+    delegate = new Delegator(transcoderAddress.toHex())
   }
 
   // Get delegate data
-  let delegateData = bondingManager.getDelegator(transcoderAddress);
+  let delegateData = bondingManager.getDelegator(transcoderAddress)
 
-  let totalStake = bondingManager.transcoderTotalStake(transcoderAddress);
+  let totalStake = bondingManager.transcoderTotalStake(transcoderAddress)
 
   // Get delegator data
-  let delegatorData = bondingManager.getDelegator(delegatorAddress);
+  let delegatorData = bondingManager.getDelegator(delegatorAddress)
 
   // Update delegate's total stake
-  transcoder.totalStake = totalStake;
+  transcoder.totalStake = totalStake
 
   // Remove delegator from delegate
-  let delegators = transcoder.delegators;
+  let delegators = transcoder.delegators
   if (delegators != null) {
-    let i = delegators.indexOf(delegatorAddress.toHex());
-    delegators.splice(i, 1);
-    transcoder.delegators = delegators;
+    let i = delegators.indexOf(delegatorAddress.toHex())
+    delegators.splice(i, 1)
+    transcoder.delegators = delegators
   }
 
   // Update delegate's delegated amount
-  delegate.delegatedAmount = delegateData.value3;
+  delegate.delegatedAmount = delegateData.value3
 
   // Remote delegate from delegator
-  delegator.delegate = null;
+  delegator.delegate = null
 
   // Update delegator's start round
-  delegator.startRound = delegatorData.value4.toString();
-
-  // Update delegator's last claim round
-  delegator.lastClaimRound = currentRound.toString();
-
-  // Update delegator's bonded amount
-  delegator.bondedAmount = delegatorData.value0;
-
-  // Update delegator's fees
-  delegator.fees = delegatorData.value1;
-
-  // Update delegator's pending stake
-  delegator.pendingStake = BigInt.fromI32(0);
-
-  // Update delegator's pending fees
-  delegator.pendingFees = BigInt.fromI32(0);
+  delegator.startRound = delegatorData.value4.toString()
 
   // Apply store updates
-  delegate.save();
-  delegator.save();
-  transcoder.save();
+  delegate.save()
+  delegator.save()
+  transcoder.save()
+
+  updateDelegatorWithEarnings(
+    delegator,
+    currentRound,
+    delegator.pendingStake as BigInt,
+    delegator.pendingFees as BigInt
+  )
 }
 
 // Handler for WithdrawStake events
 export function withdrawStake(event: WithdrawStake): void {
-  let delegatorAddress = event.params.delegator;
-  let delegator = Delegator.load(delegatorAddress.toHex());
-  delegator.bondedAmount = BigInt.fromI32(0);
-  delegator.save();
+  let delegatorAddress = event.params.delegator
+  let delegator = Delegator.load(delegatorAddress.toHex())
+  delegator.bondedAmount = BigInt.fromI32(0)
+  delegator.save()
+}
+
+export function updateDelegatorWithEarnings(
+  delegator: Delegator,
+  endRound: BigInt,
+  bondedAmount: BigInt,
+  fees: BigInt
+): void {
+  let delegateAddress = Address.fromString(delegator.delegate)
+  let currentRound = roundsManager.currentRound()
+
+  delegator.bondedAmount = bondedAmount
+  delegator.fees = fees
+  delegator.lastClaimRound = endRound.toString()
+
+  if (endRound.toI32() == currentRound.toI32()) {
+    delegator.pendingStake = BigInt.fromI32(0)
+    delegator.pendingFees = BigInt.fromI32(0)
+
+    let poolId = makePoolId(delegateAddress, endRound)
+    let pool = Pool.load(poolId) as Pool
+    if (pool == null) {
+      pool = new Pool(poolId)
+    }
+
+    // Reduce the pool's claimableStake if delegator claimed before delegate
+    // called reward
+    if (!pool.rewardTokens) {
+      pool.claimableStake = pool.claimableStake.minus(
+        delegator.bondedAmount as BigInt
+      )
+      pool.save()
+    }
+  }
+
+  delegator.save()
+}
+
+export function getPendingStakeAndFees(
+  delegator: Delegator,
+  endRound: BigInt
+): Array<BigInt> {
+  let delegateAddress = Address.fromString(delegator.delegate)
+  let currentBondedAmount = delegator.bondedAmount as BigInt
+  let currentFeesAmount = delegator.fees as BigInt
+  let share: Share
+  let shareId: string
+
+  for (
+    let i = parseInt(delegator.lastClaimRound, 10) + 1;
+    i <= endRound.toI32();
+    i++
+  ) {
+    shareId = makeShareId(delegateAddress, BigInt.fromI32(i as i32))
+    share = Share.load(shareId) as Share
+    currentBondedAmount.plus(share.rewardTokens as BigInt)
+    currentFeesAmount.plus(share.fees as BigInt)
+  }
+
+  return [currentBondedAmount, currentFeesAmount]
 }
--- packages/subgraph/src/mappings/roundsManager.ts
+++ packages/subgraph/src/mappings/roundsManager.ts
@@ -1,43 +1,43 @@
 // Import types and APIs from graph-ts
-import { Address } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from '@graphprotocol/graph-ts'
 
 // Import event types from the registrar contract ABIs
-import { RoundsManager, NewRound } from "../types/RoundsManager/RoundsManager";
-import { BondingManager } from "../types/BondingManager_LIP11/BondingManager";
+import { RoundsManager, NewRound } from '../types/RoundsManager/RoundsManager'
+import { BondingManager } from '../types/BondingManager_LIP11/BondingManager'
 
 // Import entity types generated from the GraphQL schema
-import { Transcoder, Pool, Round } from "../types/schema";
+import { Transcoder, Pool, Round } from '../types/schema'
 
-import { makePoolId } from "./util";
+import { makePoolId } from './util'
 
 // Bind BondingManager contract
 let bondingManager = BondingManager.bind(
-  Address.fromString("511bc4556d823ae99630ae8de28b9b80df90ea2e")
-);
+  Address.fromString('511bc4556d823ae99630ae8de28b9b80df90ea2e')
+)
 
 // Handler for NewRound events
 export function newRound(event: NewRound): void {
-  let roundsManager = RoundsManager.bind(event.address);
-  let roundNumber = event.params.round;
+  let roundsManager = RoundsManager.bind(event.address)
+  let roundNumber = event.params.round
   let EMPTY_ADDRESS = Address.fromString(
-    "0000000000000000000000000000000000000000"
-  );
-  let currentTranscoder = bondingManager.getFirstTranscoderInPool();
-  let transcoder = Transcoder.load(currentTranscoder.toHex());
-  let active: boolean;
-  let poolId: string;
-  let pool: Pool;
-  let round: Round;
+    '0000000000000000000000000000000000000000'
+  )
+  let currentTranscoder = bondingManager.getFirstTranscoderInPool()
+  let transcoder = Transcoder.load(currentTranscoder.toHex())
+  let active: boolean
+  let poolId: string
+  let pool: Pool
+  let round: Round
 
   // Iterate over all registered transcoders
   while (EMPTY_ADDRESS.toHex() != currentTranscoder.toHex()) {
     // Update transcoder active state
-    active = bondingManager.isActiveTranscoder(currentTranscoder, roundNumber);
-    transcoder.active = active;
-    transcoder.rewardCut = transcoder.pendingRewardCut;
-    transcoder.feeShare = transcoder.pendingFeeShare;
-    transcoder.pricePerSegment = transcoder.pendingPricePerSegment;
-    transcoder.save();
+    active = bondingManager.isActiveTranscoder(currentTranscoder, roundNumber)
+    transcoder.active = active
+    transcoder.rewardCut = transcoder.pendingRewardCut
+    transcoder.feeShare = transcoder.pendingFeeShare
+    transcoder.pricePerSegment = transcoder.pendingPricePerSegment
+    transcoder.save()
 
     // create a unique "pool" for each active transcoder on every
     // round. If a transcoder calls reward() for a given round, we store its
@@ -45,30 +45,34 @@ export function newRound(event: NewRound): void {
     // "rewardTokens" is null for a given transcoder and round then we know
     // the transcoder failed to call reward()
     if (active) {
-      poolId = makePoolId(currentTranscoder, roundNumber);
-      pool = new Pool(poolId);
-      pool.round = roundNumber.toString();
-      pool.transcoder = currentTranscoder.toHex();
+      poolId = makePoolId(currentTranscoder, roundNumber)
+      pool = new Pool(poolId)
+      pool.round = roundNumber.toString()
+      pool.transcoder = currentTranscoder.toHex()
+      pool.totalStake = transcoder.totalStake
+      pool.claimableStake = transcoder.totalStake
+      pool.rewardCut = transcoder.rewardCut
+      pool.feeShare = transcoder.feeShare
 
       // Apply store updates
-      pool.save();
+      pool.save()
     }
 
     currentTranscoder = bondingManager.getNextTranscoderInPool(
       currentTranscoder
-    );
+    )
 
-    transcoder = Transcoder.load(currentTranscoder.toHex());
+    transcoder = Transcoder.load(currentTranscoder.toHex())
   }
 
   // Create new round
-  round = new Round(roundNumber.toString());
-  round.initialized = true;
-  round.timestamp = event.block.timestamp;
-  round.lastInitializedRound = roundsManager.lastInitializedRound();
-  round.length = roundsManager.roundLength();
-  round.startBlock = roundsManager.currentRoundStartBlock();
+  round = new Round(roundNumber.toString())
+  round.initialized = true
+  round.timestamp = event.block.timestamp
+  round.lastInitializedRound = roundsManager.lastInitializedRound()
+  round.length = roundsManager.roundLength()
+  round.startBlock = roundsManager.currentRoundStartBlock()
 
   // Apply store updates
-  round.save();
+  round.save()
 }
