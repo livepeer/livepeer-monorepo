b2c2ebefcfa8cff1fb3b6002d060f3adc179fe8b
adamsoffer <adam@livepeer.org>
2019-09-11 22:40:35 -0400

    fix share calculation

--- packages/subgraph/src/mappings/util.ts
+++ packages/subgraph/src/mappings/util.ts
@@ -1,11 +1,13 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+
+const PERC_DIVISOR = 1000000
 
 // Make a number the specified number of digits
 export function leftPad(str: string, size: i32): string {
   while (str.length < size) {
-    str = "0" + str;
+    str = '0' + str
   }
-  return str;
+  return str
 }
 
 // Make a derived pool ID from a transcoder address
@@ -13,7 +15,7 @@ export function makePoolId(
   transcoderAddress: Address,
   roundId: BigInt
 ): string {
-  return transcoderAddress.toHex() + "-" + leftPad(roundId.toString(), 40);
+  return transcoderAddress.toHex() + '-' + leftPad(roundId.toString(), 40)
 }
 
 // Make a derived share ID from a delegator address
@@ -21,7 +23,7 @@ export function makeShareId(
   delegatorAddress: Address,
   roundId: BigInt
 ): string {
-  return delegatorAddress.toHex() + "-" + leftPad(roundId.toString(), 40);
+  return delegatorAddress.toHex() + '-' + leftPad(roundId.toString(), 40)
 }
 
 // Make a derived unlocking ID from a delegator address
@@ -30,6 +32,24 @@ export function makeUnbondingLockId(
   unbondingLockId: BigInt
 ): string {
   return (
-    delegatorAddress.toHex() + "-" + leftPad(unbondingLockId.toString(), 40)
-  );
+    delegatorAddress.toHex() + '-' + leftPad(unbondingLockId.toString(), 40)
+  )
+}
+
+export function percOfWithDenom(
+  _amount: BigInt,
+  _fracNum: BigInt,
+  _fracDenom: BigInt
+): BigInt {
+  return _amount
+    .times(percPoints(_fracNum, _fracDenom))
+    .div(BigInt.fromI32(PERC_DIVISOR))
+}
+
+export function percOf(_amount: BigInt, _fracNum: BigInt): BigInt {
+  return _amount.times(_fracNum).div(BigInt.fromI32(PERC_DIVISOR))
+}
+
+export function percPoints(_fracNum: BigInt, _fracDenom: BigInt): BigInt {
+  return _fracNum.times(BigInt.fromI32(PERC_DIVISOR)).div(_fracDenom)
 }
