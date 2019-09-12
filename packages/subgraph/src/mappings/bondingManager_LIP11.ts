8c699e257fe77340e01d98ff4c45446c21b42ddd
adamsoffer <adam@livepeer.org>
2019-09-12 10:39:20 -0400

    fix getPendingStakeAndFees; some refactoring

--- packages/subgraph/src/mappings/bondingManager_LIP11.ts
+++ packages/subgraph/src/mappings/bondingManager_LIP11.ts
@@ -4,119 +4,24 @@ import { Address, BigInt, store } from '@graphprotocol/graph-ts'
 // Import event types from the registrar contract ABIs
 import {
   BondingManager,
-  TranscoderUpdate,
-  TranscoderResigned,
-  TranscoderEvicted,
-  TranscoderSlashed,
-  ClaimEarningsCall,
   WithdrawStake,
-  WithdrawFees,
   Bond,
   Unbond,
-  Rebond,
-  Reward as RewardEvent // alias Reward event to avoid name collision with entity type
+  Rebond
 } from '../types/BondingManager_LIP11/BondingManager'
 import { RoundsManager } from '../types/RoundsManager/RoundsManager'
 
-import {
-  updateDelegatorWithEarnings,
-  getPendingStakeAndFees
-} from './bondingManager'
+import { updateDelegatorWithEarnings } from './bondingManager'
 
 // Import entity types generated from the GraphQL schema
-import {
-  Transcoder,
-  Pool,
-  Delegator,
-  Share,
-  UnbondingLock
-} from '../types/schema'
+import { Transcoder, Delegator, UnbondingLock } from '../types/schema'
 
-import {
-  makePoolId,
-  makeShareId,
-  makeUnbondingLockId,
-  percOf,
-  percOfWithDenom
-} from './util'
+import { makeUnbondingLockId } from './util'
 
 // Bind RoundsManager contract
 let roundsManager = RoundsManager.bind(
   Address.fromString('3984fc4ceeef1739135476f625d36d6c35c40dc3')
 )
-
-// Handler for TranscoderUpdate events
-export function transcoderUpdated(event: TranscoderUpdate): void {
-  // Bind BondingManager contract
-  let bondingManager = BondingManager.bind(event.address)
-  let currentRound = roundsManager.currentRound()
-  let transcoderAddress = event.params.transcoder
-  let transcoder = Transcoder.load(transcoderAddress.toHex())
-
-  // Create transcoder if it does not yet exist
-  if (transcoder == null) {
-    transcoder = new Transcoder(transcoderAddress.toHex())
-  }
-
-  let active = bondingManager.isActiveTranscoder(
-    transcoderAddress,
-    currentRound
-  )
-  let registered = event.params.registered
-  let pendingRewardCut = event.params.pendingRewardCut
-  let pendingFeeShare = event.params.pendingFeeShare
-  let pendingPricePerSegment = event.params.pendingPricePerSegment
-
-  // Update transcoder
-  transcoder.pendingRewardCut = pendingRewardCut
-  transcoder.pendingFeeShare = pendingFeeShare
-  transcoder.pendingPricePerSegment = pendingPricePerSegment
-  transcoder.active = active
-  transcoder.status = registered ? 'Registered' : 'NotRegistered'
-
-  // Apply store updates
-  transcoder.save()
-}
-
-// Handler for TranscoderResigned events
-export function transcoderResigned(event: TranscoderResigned): void {
-  let transcoderAddress = event.params.transcoder
-  let transcoder = Transcoder.load(transcoderAddress.toHex())
-
-  // Update transcoder
-  transcoder.active = false
-  transcoder.status = 'NotRegistered'
-
-  // Apply store updates
-  transcoder.save()
-}
-
-// Handler for TranscoderEvicted events
-export function transcoderEvicted(event: TranscoderEvicted): void {
-  let transcoderAddress = event.params.transcoder
-  let transcoder = Transcoder.load(transcoderAddress.toHex())
-
-  // Update transcoder
-  transcoder.active = false
-  transcoder.status = 'NotRegistered'
-
-  // Apply store updates
-  transcoder.save()
-}
-
-// Handler for TranscoderSlashed events
-export function transcoderSlashed(event: TranscoderSlashed): void {
-  let transcoderAddress = event.params.transcoder
-  let transcoder = Transcoder.load(transcoderAddress.toHex())
-  let bondingManager = BondingManager.bind(event.address)
-  let totalStake = bondingManager.transcoderTotalStake(transcoderAddress)
-
-  // Update transcoder total stake
-  transcoder.totalStake = totalStake
-
-  // Apply store updates
-  transcoder.save()
-}
 
 // Handler for Bond events
 export function bond(event: Bond): void {
@@ -342,122 +247,6 @@ export function rebond(event: Rebond): void {
   updateDelegatorWithEarnings(delegator, currentRound, bondedAmount, fees)
 }
 
-// Handler for Reward events
-export function reward(event: RewardEvent): void {
-  let bondingManager = BondingManager.bind(event.address)
-  let transcoderAddress = event.params.transcoder
-  let transcoder = Transcoder.load(transcoderAddress.toHex())
-  let delegate = Delegator.load(transcoderAddress.toHex())
-  let totalStake = bondingManager.transcoderTotalStake(transcoderAddress)
-  let currentRound = roundsManager.currentRound()
-  let delegateData = bondingManager.getDelegator(transcoderAddress)
-  let rewardTokens = event.params.amount
-  let shares = new Array<string>()
-
-  let transcoderRewardPool = percOf(
-    rewardTokens,
-    transcoder.rewardCut as BigInt
-  )
-  let delegatorRewardPool = rewardTokens.minus(transcoderRewardPool)
-
-  let poolId = makePoolId(transcoderAddress, currentRound)
-
-  // Get pool
-  let pool = new Pool(poolId)
-
-  // Update delegate's delegated amount
-  delegate.delegatedAmount = delegateData.value3
-
-  // Update reward tokens
-  pool.rewardTokens = event.params.amount
-
-  let delegatorAddress: Address
-  let delegator: Delegator
-  let share: Share
-  let delegators: Array<string> = transcoder.delegators as Array<string>
-  let shareId: string
-  let delegatorTotalStake: BigInt
-  let delegatorRewardShare: BigInt
-  let pendingStakeAndFees: Array<BigInt>
-
-  // Calculate each delegator's reward tokens for this round
-  // and update its pending stake
-  for (let i = 0; i < delegators.length; i++) {
-    delegatorAddress = Address.fromString(delegators[i])
-    delegator = Delegator.load(delegators[i]) as Delegator
-
-    // no rewards for delegator if it claimed earnings before transcoder
-    // called reward
-    if (parseInt(delegator.lastClaimRound, 10) >= currentRound.toI32()) {
-      continue
-    }
-
-    pendingStakeAndFees = getPendingStakeAndFees(delegator, currentRound)
-
-    delegatorTotalStake = BigInt.compare(
-      delegator.bondedAmount as BigInt,
-      pendingStakeAndFees[0] as BigInt
-    )
-      ? (delegator.bondedAmount as BigInt)
-      : (pendingStakeAndFees[0] as BigInt)
-
-    delegatorRewardShare = percOfWithDenom(
-      delegatorRewardPool,
-      delegatorTotalStake,
-      pool.claimableStake as BigInt
-    )
-
-    shareId = makeShareId(delegatorAddress, currentRound)
-
-    share = new Share(shareId)
-    share.rewardTokens = delegatorRewardShare
-    share.round = currentRound.toString()
-    share.delegator = delegatorAddress.toHex()
-    share.save()
-
-    if (pool.shares == null) {
-      pool.shares = new Array<string>()
-    }
-
-    // Add share to pool
-    shares = pool.shares as Array<string>
-    shares.push(shareId)
-    pool.shares = shares
-
-    if (transcoderAddress == delegatorAddress) {
-      delegator.pendingStake = delegatorTotalStake
-        .plus(delegatorRewardShare)
-        .plus(transcoderRewardPool)
-    } else {
-      delegator.pendingStake = delegatorTotalStake.plus(delegatorRewardShare)
-    }
-
-    delegator.save()
-  }
-
-  // Update transcoder total stake and last reward round
-  transcoder.totalStake = totalStake
-  transcoder.lastRewardRound = currentRound.toString()
-
-  transcoder.save()
-  delegate.save()
-  pool.save()
-}
-
-export function claimEarnings(call: ClaimEarningsCall): void {
-  let delegatorAddress = call.transaction.from
-  let endRound = call.inputs._endRound
-  let delegator = Delegator.load(delegatorAddress.toHex()) as Delegator
-  let bondingManager = BondingManager.bind(call.to)
-  let delegatorData = bondingManager.getDelegator(
-    Address.fromString(delegator.id)
-  )
-
-  let bondedAmount = delegatorData.value0
-  let fees = delegatorData.value1
-  updateDelegatorWithEarnings(delegator, endRound, bondedAmount, fees)
-}
-
 // Handler for WithdrawStake events
 export function withdrawStake(event: WithdrawStake): void {
   let delegatorAddress = event.params.delegator
@@ -467,17 +256,4 @@ export function withdrawStake(event: WithdrawStake): void {
     unbondingLockId
   )
   store.remove('UnbondingLock', uniqueUnbondingLockId)
-}
-
-export function withdrawFees(event: WithdrawFees): void {
-  let delegatorAddress = event.params.delegator
-  let delegator = Delegator.load(delegatorAddress.toHex()) as Delegator
-  let currentRound = roundsManager.currentRound()
-
-  updateDelegatorWithEarnings(
-    delegator,
-    currentRound,
-    delegator.pendingStake as BigInt,
-    delegator.pendingFees as BigInt
-  )
 }
