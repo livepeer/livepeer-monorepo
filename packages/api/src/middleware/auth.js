b2aadabc46deed684725ff280f13d77d205460b5
Yahya <ya7yaz@gmail.com>
2019-09-23 21:23:43 +0200

    chore: cleaning up the unused functions

--- packages/api/src/middleware/auth.js
+++ packages/api/src/middleware/auth.js
@@ -8,40 +8,16 @@ const router = Router()
 router.use(bearerToken())
 
 /**
- * verify the http brearer token
- * NOTE: this generates a token if no token is provided for now
- * FIXME: stop generating tokens on the fly
- * @param {object} req req object, this should already have req.store
- * @param {object} res res object
- * @param {fn} next callback
- */
-async function verifyToken(req, res, next) {
-  // if (!req || !req.token) {
-  //   return res.sendStatus(403)
-  // }
-  let resp
-  try {
-    // check token against token DB
-    resp = await req.store.get(`apitoken/${req.token}`)
-  } catch (e) {
-    logger.warn('api Token not found... generating one')
-    await generateToken(req, res, next)
-  }
-}
-
-/**
  * generate token middleware
  * @param {object} req req object
  * @param {object} res res object
  * @param {fn} next callback
  */
 async function generateToken(req, res, next) {
-  const id = uuid()
+  const id = req.token
   let resp = await req.store.create({
     id,
     kind: 'apitoken',
-    otherdata: 1,
-    autogen: true,
   })
 
   req.token = id
@@ -49,8 +25,10 @@ async function generateToken(req, res, next) {
   return next()
 }
 
-// router.use(verifyToken)
-
+/**
+ * creates an authentication middleware that can be customized.
+ * @param {Object} params auth middleware params, to be defined later
+ */
 function authFactory(params) {
   return router.use(async (req, res, next) => {
     if (!req || !req.token) {
@@ -62,6 +40,10 @@ function authFactory(params) {
       // check token against token DB
       resp = await req.store.get(`apitoken/${req.token}`)
     } catch (e) {
+      if (e.type !== 'NotFoundError') {
+        throw e
+      }
+
       logger.warn('api Token not found... generating one')
       return await generateToken(req, res, next)
     }
