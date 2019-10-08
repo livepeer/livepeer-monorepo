f44e2dae60c26ae00aebd290ac62e3c2e7ef1cef
Eli Mallon <eli@livepeer.org>
2019-10-08 03:00:35 +0000

    feat(api): add /api/orchestrator/ext/:token (#482)

--- packages/api/src/controllers/orchestrator.js
+++ packages/api/src/controllers/orchestrator.js
@@ -2,10 +2,13 @@ import { Router } from 'express'
 
 const app = Router()
 
-app.get('/', async (req, res, next) => {
+const getOrchestrators = async (req, res, next) => {
   const orchestrators = await req.getOrchestrators(req)
 
   return res.json(orchestrators.map(({ address }) => ({ address })))
-})
+}
+
+app.get('/', getOrchestrators)
+app.get('/ext/:token', getOrchestrators)
 
 export default app
