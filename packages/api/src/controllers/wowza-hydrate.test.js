8892ee9c07c6b3227dad979e4863d24c2b6f6eeb
Angie Ramirez <angie@livepeer.org>
2019-10-10 12:12:31 -0400

    Adding ability for SourceStreamName transcoding settings handling (#483)

--- packages/api/src/controllers/wowza-hydrate.test.js
+++ packages/api/src/controllers/wowza-hydrate.test.js
@@ -4,13 +4,27 @@ const stream = require('./wowza-hydrate.test-data.json')
 describe('wowzaHydrate', () => {
   it('should correctly determine renditions and presets', () => {
     const newStream = wowzaHydrate({ ...stream })
-    expect(newStream.presets).toEqual(['P360p30fps16x9', 'P144p30fps16x9'])
+    expect(newStream.presets).toEqual(['P360p30fps16x9', 'P240p30fps4x3'])
     expect(newStream.renditions).toEqual({
       test_stream_source:
         '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/source.m3u8',
       test_stream_360p:
         '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P360p30fps16x9.m3u8',
-      test_stream_160p:
+      test_stream_240p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P240p30fps4x3.m3u8',
+    })
+  })
+
+  it('should correctly determine renditions and presets with different stream name', () => {
+    stream.name = 'transrate'
+    const newStream = wowzaHydrate({ ...stream })
+    expect(newStream.presets).toEqual(['P360p30fps16x9', 'P144p30fps16x9'])
+    expect(newStream.renditions).toEqual({
+      transrate_source:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/source.m3u8',
+      transrate_360p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P360p30fps16x9.m3u8',
+      transrate_160p:
         '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P144p30fps16x9.m3u8',
     })
   })
