c892af437e1ff4bfe6e34f91c21e065f9d3f4e61
Eli Mallon <eli@livepeer.org>
2019-10-10 21:39:15 -0700

    feat(api): add worker.js, wrangler config

---
+++ packages/api/src/worker.js
@@ -0,0 +1,121 @@
+/**
+ * Entrypoint for our CloudFlare worker. Eventually will have bits of the API compiled into it, for now it's
+ * just separate.
+ */
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event))
+})
+
+const API_PREFIXES = ['/stream', '/api', '/live']
+
+const startsWithApiPrefix = pathname => {
+  for (const prefix of API_PREFIXES) {
+    if (pathname.startsWith(prefix)) {
+      return true
+    }
+  }
+  return false
+}
+
+const geolocate = async (first = false) => {
+  const servers = ['dsm.livepeer.live', 'ams.livepeer.live']
+
+  let smallestServer
+  let smallestDuration = Infinity
+
+  const promises = servers.map(async server => {
+    const start = Date.now()
+    const res = await fetch(`https://${server}/api`)
+    const duration = Date.now() - start
+    if (duration < smallestDuration) {
+      smallestDuration = duration
+      smallestServer = server
+    }
+    return {
+      server,
+      duration,
+    }
+  })
+  let data
+  if (first) {
+    data = await Promise.race(promises)
+  } else {
+    data = await Promise.all(promises)
+  }
+  const ret = {
+    chosenServer: smallestServer,
+    servers: data,
+  }
+  return ret
+}
+
+/**
+ * Serve a static asset or 404
+ */
+async function serveStaticAsset(event) {
+  try {
+    return await getAssetFromKV(event)
+  } catch (e) {
+    let pathname = new URL(event.request.url).pathname
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: 'not found',
+    })
+  }
+}
+
+/**
+ * Fetch and log a request
+ * @param {Request} request
+ */
+async function handleEvent(event) {
+  const req = event.request
+  const url = new URL(req.url)
+  if (url.pathname.startsWith('/.well-known/acme-challenge')) {
+    const newUrl = new URL(req.url)
+    newUrl.hostname = 'ams.livepeer.live'
+    newUrl.protocol = 'https:'
+    newUrl.port = 443
+    const newRequest = new Request(newUrl.toString(), new Request(req))
+    return fetch(newRequest)
+  }
+  if (url.hostname === 'docs.livepeer.live') {
+    if (url.pathname === '/') {
+      return fetch('http://docs.livepeer.live/index.html')
+    }
+  }
+  if (url.hostname === 'build.livepeer.live') {
+    return fetch(req)
+  }
+  if (url.hostname === 'livepeer.live') {
+    if (url.pathname === '/api/geolocate') {
+      const data = await geolocate(false)
+      const res = new Response(JSON.stringify(data, null, 2), {
+        headers: { 'content-type': 'application/json' },
+      })
+      return res
+    }
+    if (url.pathname === '/test.mp4') {
+      return Response.redirect(
+        'https://storage.googleapis.com/lp_testharness_assets/official_test_source_2s_keys_24pfs.mp4',
+        302,
+      )
+    }
+    // Serve the front-end to non-api stuff
+    if (!startsWithApiPrefix(url.pathname)) {
+      if (url.protocol === 'http:') {
+        return Response.redirect(`https://${url.hostname}${url.pathname}`, 302)
+      }
+      return serveStaticAsset(event)
+    }
+  }
+  const newUrl = new URL(req.url)
+  const { chosenServer } = await geolocate(true)
+  newUrl.hostname = chosenServer
+  newUrl.protocol = 'https:'
+  newUrl.port = 443
+  const newRequest = new Request(newUrl.toString(), new Request(req))
+  return fetch(newRequest)
+}
