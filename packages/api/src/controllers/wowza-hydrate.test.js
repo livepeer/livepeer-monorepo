bc66a9bb916ea93008ab89338303767481441216
Angie Ramirez <angie@livepeer.org>
2019-10-29 15:14:48 -0400

    Adding proper handling to incoming width/height 0 from wowza templates (#486)
    * correcting for width/height 0 in wowza templates
    
    * adding test changes

--- packages/api/src/controllers/wowza-hydrate.test.js
+++ packages/api/src/controllers/wowza-hydrate.test.js
@@ -1,5 +1,8 @@
 import wowzaHydrate from './wowza-hydrate'
 const stream = require('./wowza-hydrate.test-data.json')
+const streamWithoutTransrate = JSON.parse(JSON.stringify(stream))
+streamWithoutTransrate.wowza.transcoderAppConfig.templatesInUse =
+  '${SourceStreamName}.xml'
 
 describe('wowzaHydrate', () => {
   it('should correctly determine renditions and presets', () => {
@@ -27,6 +30,40 @@ describe('wowzaHydrate', () => {
       transrate_160p:
         '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P144p30fps16x9.m3u8',
     })
+  })
+
+  it('should correctly substitute heights widths to correct value', () => {
+    stream.name = 'width_height_test'
+    const newStream = wowzaHydrate({ ...stream })
+    expect(newStream.presets).toEqual([
+      'P720p60fps16x9',
+      'P360p30fps16x9',
+      'P240p30fps16x9',
+      'P144p30fps16x9',
+    ])
+    expect(newStream.renditions).toEqual({
+      width_height_test_source:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/source.m3u8',
+      width_height_test_720p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P720p60fps16x9.m3u8',
+      width_height_test_360p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P360p30fps16x9.m3u8',
+      width_height_test_240p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P240p30fps16x9.m3u8',
+      width_height_test_160p:
+        '/stream/de7818e7-610a-4057-8f6f-b785dc1e6f88/P144p30fps16x9.m3u8',
+    })
+  })
+
+  it('should fail if stream name does not equal any template name', () => {
+    let message
+    streamWithoutTransrate.name = 'fake_name'
+    try {
+      const newStream = wowzaHydrate({ ...streamWithoutTransrate })
+    } catch (e) {
+      message = e.message
+    }
+    expect(message).toEqual('no template found from templatesInUse')
   })
 
   it('should fail if missing parameters', () => {
