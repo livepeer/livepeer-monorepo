45deb153732790ced49fbe5f5ed46c5a081bdbcb
Adam Soffer <adam@soffer.dev>
2019-11-19 16:46:48 -0500

    add postinstall to circleci.yml

--- packages/explorer-2.0/next.config.js
+++ packages/explorer-2.0/next.config.js
@@ -8,9 +8,6 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'graphql-tag/loader',
     })
-    // https://spectrum.chat/zeit/general/error-while-building-project~cf3cadfd-6370-4dae-8399-84c8ab2b4841?m=MTU2MDI1NTY2NDYxNw==
-
-    //config.resolve.alias['scrypt.js'] = path.resolve('scryptsy')
     config.plugins.push(new webpack.IgnorePlugin(/^(?:electron|ws)$/))
     return config
   },
