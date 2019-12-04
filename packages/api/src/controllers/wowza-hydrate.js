705bc4fa7715d79f8a5cc3eaf29e4004807201db
Angie Ramirez <angie@livepeer.org>
2019-12-04 15:42:53 -0500

    adding the ability to handle passthrough custom profiles

--- packages/api/src/controllers/wowza-hydrate.js
+++ packages/api/src/controllers/wowza-hydrate.js
@@ -1,7 +1,5 @@
 import { VIDEO_PROFILES } from '@livepeer/sdk'
 
-const PRESETS = Object.values(VIDEO_PROFILES)
-
 /**
  * Replace a string containing "${SourceStreamName}" with the appropriate stream name
  */
@@ -9,7 +7,7 @@ const replaceStreamName = (str, name) =>
   str.replace('${SourceStreamName}', name)
 
 /**
- * Take a data blob from Wowza and return the appropriate presets and names
+ * Take a data blob and return the appropriate profiles and names
  */
 export default stream => {
   if (!stream.wowza) {
@@ -40,11 +38,11 @@ export default stream => {
   const transcoderConfig = transcoderTemplateAppConfig[template]
   const enabledEncodes = transcoderConfig.encodes.filter(e => e.enable)
   const renditions = {}
-  let presets = []
+  let profiles = []
   const encodeNameToRenditionName = {}
   const aspectRatio = sourceInfo.width / sourceInfo.height
   for (const encode of enabledEncodes) {
-    let { width, height, name, streamName, videoCodec } = encode
+    let { width, height, name, streamName, videoCodec, videoBitrate } = encode
     let renditionName = replaceStreamName(streamName, stream.name)
     // These can be of the form mp4:name, let's ignore the first bit if present
     if (renditionName.includes(':')) {
@@ -58,40 +56,21 @@ export default stream => {
 
     if (width === 0) width = height * aspectRatio
     if (height === 0) height = (width * 1) / aspectRatio
-    // TODO: do we need to validate whether or not the incoming height/width fit the aspect ratio?
 
-    const wowzaSize = width * height
-    const wowzaFps = sourceInfo.fps
-    let diff = Infinity
-    let fpsDiff = Infinity
-    let foundPreset = null
-
-    for (const preset of PRESETS) {
-      const [livepeerWidth, livepeerHeight] = preset.resolution
-        .split('x')
-        .map(x => parseInt(x))
-      const livepeerSize = livepeerWidth * livepeerHeight
-      const thisDiff = Math.abs(livepeerSize - wowzaSize)
-      const thisFpsDiff = Math.abs(preset.framerate - wowzaFps)
-      if (thisDiff < diff || (thisDiff == diff && thisFpsDiff < fpsDiff)) {
-        foundPreset = preset.name
-        diff = thisDiff
-        fpsDiff = thisFpsDiff
-      }
+    const profile = {
+      name: renditionName,
+      height: height,
+      width: width,
+      bitrate: parseInt(videoBitrate),
+      fps: sourceInfo.fps, // NOTE: or is this the `interval` field?
     }
 
-    if (!foundPreset) {
-      throw new Error(
-        `couldn't find Livepeer preset for Wowza encode: ${JSON.stringify(
-          encode,
-        )}`,
-      )
-    }
-    renditions[renditionName] = `/stream/${stream.id}/${foundPreset}.m3u8`
-    presets.push(foundPreset)
+    renditions[renditionName] = `/stream/${stream.id}/${renditionName}.m3u8`
+    profiles.push(profile)
   }
-  // Dedupe presets
-  presets = [...new Set(presets)]
+
+  // Dedupe profiles
+  profiles = [...new Set(profiles)]
   const enabledEncodeNames = new Set(enabledEncodes.map(encode => encode.name))
   const streamNameGroups = transcoderConfig.streamNameGroups.map(
     streamNameGroup => {
@@ -102,6 +81,7 @@ export default stream => {
       return { name, renditions }
     },
   )
+  const presets = []
   return {
     ...stream,
     wowza: {
@@ -109,6 +89,7 @@ export default stream => {
       streamNameGroups,
     },
     renditions,
+    profiles,
     presets,
   }
 }
