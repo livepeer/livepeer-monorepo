17d6f36b5bedbbccf9587a632b50864da0b6a35c
Angie Ramirez <angie@livepeer.org>
2020-02-18 16:10:44 -0500

    adding ability to query GraphQL

--- packages/api/src/controllers/broadcaster.js
+++ packages/api/src/controllers/broadcaster.js
@@ -1,6 +1,8 @@
 import Router from 'express/lib/router'
 import fetch from 'isomorphic-fetch'
 import { authMiddleware } from '../middleware'
+import { request } from 'graphql-request'
+
 
 const app = Router()
 
@@ -30,6 +32,7 @@ app.get('/addresses', async (req, res, next) => {
   const broadcasters = await req.getBroadcasters(req)
   const ethAddresses = {}
   for (const broadcaster of broadcasters) {
+    // Fetch Eth Addresses of all available Broadcasters
     const addrRes = await fetch(`${broadcaster.cliAddress}/ethAddr`)
 
     let ethAddr
@@ -44,6 +47,23 @@ app.get('/addresses', async (req, res, next) => {
       ethAddresses[broadcaster.address] = `${ethAddr}`
     }
   }
+
+  // Request funds of all available Broadcasters using their Eth Addresses
+  const addresses = Object.keys(ethAddresses)
+  const graphUrl = 'https://api.thegraph.com/subgraphs/name/livepeer/livepeer'
+  const query = `{
+    broadcasters(where: {id_in: ["${addresses}"]}) {
+      balance
+    }
+  }`
+
+  request(graphUrl, query)
+    // TO DO: once GraphQL supports this query, parse through data, add to ethAddresses object (renamed),
+    // and return in res.json. Perhaps also refactor this bit into another function. Confirmed: request works, though errs.
+    .then(broadcasterFunds => console.log(`broadcaster wallet funds: ${JSON.stringify(broadcasterFunds)}`))
+    .catch(e => {
+      console.error(`Error fetching data from GraphQL: ${e}`)
+    })
 
   res.json(ethAddresses)
 })
