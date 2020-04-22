0b0c76f4eb69a4b4431f8a11954dd3a4c9d9213c
ya7ya <ya7yaz@gmail.com>
2020-04-22 14:57:24 +0200

    simple lastSeen tracking

---
+++ packages/api/src/middleware/tracking.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// function trackingFactory(params) {
+//   return async (req, res, next) => {
+//     let authToken = req.headers.authorization
+//     if (!authToken) {
+//       // TODO log error here
+//     }
+
+
+//     console.log('tracking: ', req.user, req.headers.authorization)
+//     return next()
+//   }
+// }
+
+// export default trackingFactory
+
+const tracking = {
+  record: function record (store, tokenObject) {
+    tokenObject.lastSeen = new Date()
+    store.replace(tokenObject).then((_) => {
+      // all good
+      console.log('all good')
+    }).catch((e) => {
+      // TODO Log error 
+    })
+  }
+}
+
+export default tracking
