f85b448314b3472a0806a1f915fffb39853af5c5
Adam Soffer <adam@livepeer.org>
2020-04-22 13:31:07 -0400

    replace system-ui font with Inter (#641)

--- packages/livepeer.com/www/lib/theme.tsx
+++ packages/livepeer.com/www/lib/theme.tsx
@@ -6,7 +6,7 @@ export const theme = {
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
     body:
-      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+      'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
     heading: "inherit",
     monospace: "Menlo, monospace"
   },
