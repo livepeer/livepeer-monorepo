8254c34b6f0cb4f185a8274f4f39fbc74ef84bc4
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-04-29 23:44:10 +0300

    Streams table

--- packages/livepeer.com/www/components/TokenTable/index.tsx
+++ packages/livepeer.com/www/components/TokenTable/index.tsx
@@ -4,8 +4,10 @@ import { Box, Button, Flex, Input } from "@theme-ui/components";
 import Modal from "../Modal";
 import { Table, TableRow, Checkbox } from "../Table";
 import CopyBox from "../CopyBox";
+import moment from 'moment'
 
 export default ({ userId, id }) => {
+  const [streams, setStreams] = useState([]);
   const [tokens, setTokens] = useState([]);
   const [tokenName, setTokenName] = useState("");
   const [createModal, setCreateModal] = useState(false);
@@ -14,12 +16,25 @@ export default ({ userId, id }) => {
   const [newToken, setNewToken] = useState(null);
   const [selectedToken, setSelectedToken] = useState(null);
   const [copyTime, setCopyTime] = useState(null);
-  const { getApiTokens, createApiToken, deleteApiToken } = useApi();
+  const { getStreams, getApiTokens, createApiToken, deleteApiToken } = useApi();
   useEffect(() => {
     getApiTokens(userId)
       .then(tokens => setTokens(tokens))
       .catch(err => console.error(err)); // todo: surface this
   }, [userId, newToken, deleteModal]);
+  useEffect(() => {
+    getStreams(userId)
+      .then(streams => setStreams(streams))
+      .catch(err => console.error(err)); // todo: surface this
+  }, [userId, newToken, deleteModal]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getStreams(userId)
+        .then(streams => setStreams(streams))
+        .catch(err => console.error(err)); // todo: surface this
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
   const close = () => {
     setCreateModal(false);
     setDeleteModal(false);
@@ -198,6 +213,52 @@ export default ({ userId, id }) => {
             </TableRow>
           );
         })}
+      </Table>
+      <p>
+        Streams:
+      </p>
+      <Table sx={{ gridTemplateColumns: "auto 1fr auto auto auto" }}>
+        <TableRow variant="header">
+          <Box>ID</Box>
+          <Box>Name</Box>
+          <Box>Details</Box>
+          <Box>Segments</Box>
+          <Box>Last Active</Box>
+        </TableRow>
+        {
+          streams.map(stream => {
+            const { id, name, lastSeen, sourceSegments, transcodedSegments,
+              presets, renditions } = stream;
+            let formattedLastSeen = <em>unseen</em>;
+            if (lastSeen) {
+              formattedLastSeen = (
+                <span>{
+                  moment
+                    .unix(lastSeen / 1000.0)
+                    .fromNow()}
+                </span>
+              );
+            }
+            let details = ''
+            if (presets?.length) {
+              details = `${presets}`
+            }
+            if (Object.keys(renditions||{}).length) {
+              if (details) {
+                details += '/'
+              }
+              details += `${renditions}`
+            }
+            return (
+              <TableRow key={id} >
+                <Box>{id}</Box>
+                <Box>{name}</Box>
+                <Box>{details}</Box>
+                <Box><span>{sourceSegments || 0}/{transcodedSegments || 0}</span></Box>
+                <Box>{formattedLastSeen}</Box>
+              </TableRow>
+            );
+          })}
       </Table>
     </Box>
   );
--- packages/livepeer.com/www/hooks/use-api.tsx
+++ packages/livepeer.com/www/hooks/use-api.tsx
@@ -1,7 +1,7 @@
 import { useState, useContext, createContext, useEffect } from "react";
 import fetch from "isomorphic-fetch";
 import jwt from "jsonwebtoken";
-import { User, Error as ApiError, ApiToken } from "@livepeer/api";
+import { User, Error as ApiError, ApiToken, Stream } from "@livepeer/api";
 import qs from "qs";
 
 /**
@@ -143,6 +143,16 @@ const makeContext = (state: ApiState, setState) => {
     async logout() {
       setState(state => ({ ...state, user: null, token: null }));
       clearToken();
+    },
+
+    async getStreams(userId): Promise<[Stream]> {
+      const [res, streams] = await context.fetch(
+        `/stream?${qs.stringify({ userId })}`
+      );
+      if (res.status !== 200) {
+        throw new Error(streams);
+      }
+      return streams.sort((a, b) => (b.lastSeen||0) - (a.lastSeen||0));
     },
 
     async getApiTokens(userId): Promise<[ApiToken]> {
