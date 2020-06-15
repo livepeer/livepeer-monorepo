aa3d608f286c20d2f5e71b8b38ab6096f3d0c451
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-06-15 23:26:25 +0300

    Geolocate /ingest endpoint

--- packages/api/src/controllers/ingest.js
+++ packages/api/src/controllers/ingest.js
@@ -1,11 +1,18 @@
 import Router from 'express/lib/router'
-import { authMiddleware } from '../middleware'
+import { geolocateMiddleware } from '../middleware'
+import { amalgamate } from './broadcaster'
+import { shuffle } from '../util'
 
 const app = Router()
 
-app.get('/', async (req, res, next) => {
-  const ingest = await req.getIngest(req)
-  res.json(ingest)
+app.get('/', geolocateMiddleware({}), async (req, res, next) => {
+  let ingestPoints
+  if (req.region && req.region.servers) {
+    ingestPoints = await amalgamate(req, 'ingest')
+  } else {
+    ingestPoints = await req.getIngest(req)
+  }
+  res.json(shuffle(ingestPoints))
 })
 
 export default app
