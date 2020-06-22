8fed29ee79687d81c7721a598f818f030697ce86
Adam Soffer <adam@livepeer.org>
2020-06-22 14:25:02 -0400

    rename var

--- packages/explorer-2.0/pages/api/totalTokenSupply.tsx
+++ packages/explorer-2.0/pages/api/totalTokenSupply.tsx
@@ -1,11 +1,11 @@
 export default async (req, res) => {
-  const countdownRaw = await fetch(
+  const response = await fetch(
     `https://${
       process.env.NETWORK === 'rinkeby' ? 'api-rinkeby' : 'api'
     }.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x58b6a8a3302369daec383334672404ee733ab239&apikey=${
       process.env.ETHERSCAN_API_KEY
     }`,
   )
-  const { result: totalTokenSupply } = await countdownRaw.json()
+  const { result: totalTokenSupply } = await response.json()
   res.end(totalTokenSupply)
 }
