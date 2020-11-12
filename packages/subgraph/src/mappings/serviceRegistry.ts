fe7a7ef6256f687fa0321e04ff404bb09c1b6da4
Adam Soffer <adam@livepeer.org>
2020-11-12 10:56:50 -0800

    add serviceURI to transcoder entity

---
+++ packages/subgraph/src/mappings/serviceRegistry.ts
@@ -0,0 +1,19 @@
+// Import event types from the registrar contract ABIs
+import { ServiceURIUpdate as ServiceURIUpdateEvent } from '../types/ServiceRegistry/ServiceRegistry'
+
+// Import entity types generated from the GraphQL schema
+import {
+  Transcoder
+} from '../types/schema'
+
+export function serviceURIUpdate(event: ServiceURIUpdateEvent): void {
+  let transcoder =
+    Transcoder.load(event.params.addr.toHex()) ||
+    new Transcoder(event.params.addr.toHex())
+
+  // Update transcoder
+  transcoder.serviceURI = event.params.serviceURI
+
+  // Apply store updates
+  transcoder.save()
+}
