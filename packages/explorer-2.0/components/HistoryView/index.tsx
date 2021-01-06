4f3d2d73ba4605a5fe05039dbf35af0c142e588d
adamsoffer <adam@livepeer.org>
2021-01-05 20:14:06 -0500

    rm unnecessary destructuring of events

--- packages/explorer-2.0/components/HistoryView/index.tsx
+++ packages/explorer-2.0/components/HistoryView/index.tsx
@@ -55,7 +55,7 @@ const Index = () => {
   }
 
   const events = data.transactions.reduce(
-    (res, { events }) => res.concat(...events),
+    (res, { events }) => res.concat(events),
     [],
   )
 
