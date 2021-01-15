62ff6a2681544973d9c1b27fd2fe55706d3b92a0
Yondon Fu <yondon.fu@gmail.com>
2021-01-14 22:04:44 -0500

    subgraph: Contract call in roundLength parameterUpdate handler
    Contract call to get lastRoundLengthUpdateRound.

--- packages/subgraph/src/mappings/roundsManager.ts
+++ packages/subgraph/src/mappings/roundsManager.ts
@@ -125,13 +125,14 @@ export function parameterUpdate(event: ParameterUpdate): void {
   if (event.params.param == "roundLength") {
     let roundLength = roundsManager.roundLength();
     let lastRoundLengthUpdateStartBlock = roundsManager.lastRoundLengthUpdateStartBlock();
+    let lastRoundLengthUpdateRound = roundsManager.lastRoundLengthUpdateRound();
 
     if (protocol.roundLength.toI32() == 0) {
       createRound(event.block.number, roundLength, currentRound);
     }
     protocol.roundLength = roundLength;
     protocol.lastRoundLengthUpdateStartBlock = lastRoundLengthUpdateStartBlock;
-    protocol.lastRoundLengthUpdateRound = currentRound.toString();
+    protocol.lastRoundLengthUpdateRound = lastRoundLengthUpdateRound.toString();
     protocol.currentRound = currentRound.toString();
   }
 
