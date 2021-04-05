753859218e7ba524187872c6af9bc8f78bf9b0c3
Adam Soffer <adam@livepeer.org>
2021-04-05 10:44:16 -0400

    add back old redirects from classic explorer (#967)

--- packages/explorer-2.0/next.config.js
+++ packages/explorer-2.0/next.config.js
@@ -23,6 +23,26 @@ const nextConfig = {
         destination: "/accounts/:slug/staking",
         permanent: false,
       },
+      {
+        source: "/transcoders",
+        destination: "/orchestrators",
+        permanent: false,
+      },
+      {
+        source: "/accounts/(.*)/transcoding",
+        destination: "/accounts/$1/campaign",
+        permanent: false,
+      },
+      {
+        source: "/accounts/(.*)/delegating",
+        destination: "/accounts/$1/staking",
+        permanent: false,
+      },
+      {
+        source: "/accounts/(.*)/overview",
+        destination: "/accounts/$1/staking",
+        permanent: false,
+      },
     ];
   },
 };
