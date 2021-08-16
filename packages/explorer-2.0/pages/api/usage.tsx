c064375731bcc77da91f48aed3abdf4f105b2aea
Adam Soffer <adam@livepeer.org>
2021-08-16 12:36:40 -0400

    remove unused api endpoint (#1012)

--- packages/explorer-2.0/pages/api/usage.tsx
+++
@@ -1,9 +0,0 @@
-import { getChartData } from "../../apollo/resolvers/Query";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const usageData = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const usage = await getChartData();
-  res.json(usage);
-};
-
-export default usageData;
